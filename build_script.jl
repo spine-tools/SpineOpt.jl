7394ec67ac6be0332404729ebdb406489a118a6e
Manuel Marin <manuelma@kth.se>
2018-04-27 18:51:05 +0200

    Attempt to fix build script

--- build_script.jl
+++ build_script.jl
@@ -1,8 +1,8 @@
 ## This script ensures that there is a local copy of the package.
 Pkg.update()
-Pkg.clone(pwd())
-Pkg.checkout("SpineModel", "manuelma")
 Pkg.clone("https://gitlab.vtt.fi/spine/data.git", "SpineData")
 Pkg.checkout("SpineData", "manuelma")
 Pkg.build("SpineData")
+Pkg.clone(pwd())
+Pkg.checkout("SpineModel", "manuelma")
 Pkg.build("SpineModel")
