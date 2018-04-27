f152d124725c19368817caacfc61681b766ba25b
Manuel Marin <manuelma@kth.se>
2018-04-27 18:48:55 +0200

    Add SpineData to build script

--- build_script.jl
+++ build_script.jl
@@ -2,4 +2,7 @@
 Pkg.update()
 Pkg.clone(pwd())
 Pkg.checkout("SpineModel", "manuelma")
+Pkg.clone("https://gitlab.vtt.fi/spine/data.git", "SpineData")
+Pkg.checkout("SpineData", "manuelma")
+Pkg.build("SpineData")
 Pkg.build("SpineModel")
