f64eaadfd48da660db89aa0e9db0afe2bdd28da6
Manuel Marin <manuelma@kth.se>
2018-04-28 17:02:57 +0200

    More debugging (only way to try out the runner is pushing?

--- build_script.jl
+++ build_script.jl
@@ -1,9 +1,9 @@
 ## This script ensures that there is a local copy of the package.
 Pkg.update()
 Pkg.clone(joinpath(pwd(), "data"), "SpineData")
-Pkg.status("Graphs")
-Pkg.checkout("SpineData", "manuelma")
+Pkg.status("SpineData")
+#Pkg.checkout("SpineData", "manuelma")
 Pkg.build("SpineData")
 Pkg.clone(pwd(), "SpineModel")
-Pkg.checkout("SpineModel", "manuelma")
+#Pkg.checkout("SpineModel", "manuelma")
 Pkg.build("SpineModel")
