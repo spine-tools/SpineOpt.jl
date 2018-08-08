1e8ad86fe65237f36ffa9f7f042c0801a027b0ab
maren <maren.ihlemann@kuleuven.be>
2018-08-08 15:29:42 +0200

    excel merge, julia delete

--- julia/connected_nodes.jl
+++
@@ -1,5 +0,0 @@
-function connected_nodes(n)
-     if getkey(jfo["NodeConnectionRelationship"], n,0 ) != 0
-         jfo["NodeConnectionRelationship"][n]
-     end
- end
--- julia/constraints/constraint_outinratio.jl
+++
@@ -1,6 +0,0 @@
-function constraint_outinratio(m::Model, flow)
-    @constraint(m, [u in unit(), t=1:24; !isnull(ratio_output_input_flow(u))],
-        + sum(flow[c, u, "out", t] for c in unit_output_commodity(u))
-        == ratio_output_input_flow(u) * sum(flow[c, u, "in", t] for c in unit_input_commodity(u))
-    )
-end
