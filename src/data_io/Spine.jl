9753a97e7017d0fcbf72fd22aabfd2ae7ce22081
Manuel Marin <manuelma@kth.se>
2018-08-28 10:28:39 +0200

    Issue #41 updated. JuMP_all_out generates convenience functions to access multiD relationships and parameters.

--- src/data_io/Spine.jl
+++ src/data_io/Spine.jl
@@ -1,5 +1,5 @@
 """
-    JuMP_all_out(db_url, update_all_datatypes=true)
+    JuMP_all_out(db_url)
 
 Generate and export convenience functions
 named after each object class, relationship class, and parameter in `source`,
@@ -8,18 +8,167 @@ is a database url composed according to
 [sqlalchemy rules](http://docs.sqlalchemy.org/en/latest/core/engines.html#database-urls).
 See also: [`JuMP_all_out(mapping::PyObject, update_all_datatypes=true)`](@ref) for details
 about the generated convenience functions.
-
-If `update_all_datatypes` is `true`, then the method tries to find out the julia `Type` that best fits
-all values for every parameter in the database pointed by `db_url`,
-and converts all values to that `Type`. (See `update_all_datatypes!`.)
 """
-function JuMP_all_out(db_url, update_all_datatypes=true)
+function JuMP_all_out(db_url)
     mapping = db_api[:DatabaseMapping](db_url)
-    JuMP_all_out(mapping, update_all_datatypes)
+    JuMP_all_out(mapping)
+end
+
+
+function JuMP_object_out(mapping::PyObject)
+    object_class_list = py"$mapping.object_class_list()"
+    for object_class in py"[x._asdict() for x in $object_class_list]"
+        object_class_id = object_class["id"]
+        object_class_name = object_class["name"]
+        object_list = py"$mapping.object_list(class_id=$object_class_id)"
+        object_names = py"[x.name for x in $object_list]"
+        @suppress_err begin
+            @eval begin
+                $(Symbol(object_class_name))() = $(object_names)
+                export $(Symbol(object_class_name))
+            end
+        end
+    end
+end
+
+function JuMP_relationship_out(mapping::PyObject)
+    relationship_class_list = py"$mapping.wide_relationship_class_list()"
+    for relationship_class in py"[x._asdict() for x in $relationship_class_list]"
+        relationship_class_id = relationship_class["id"]
+        relationship_class_name = relationship_class["name"]
+        object_class_name_list = [String(x) for x in split(relationship_class["object_class_name_list"], ",")]
+        # Append an increasing integer to object classes that are repeated (eg, commodity1, commodity2)
+        object_class_name_ocurrences = Dict{String,Int64}()
+        for (i, object_class_name) in enumerate(object_class_name_list)
+            n_ocurrences = count(x -> contains(x, object_class_name), object_class_name_list)
+            n_ocurrences == 1 && continue
+            ocurrence = get(object_class_name_ocurrences, object_class_name, 1)
+            object_class_name_list[i] = string(object_class_name, ocurrence)
+            object_class_name_ocurrences[object_class_name] = ocurrence + 1
+        end
+        relationship_list = py"$mapping.wide_relationship_list(class_id=$relationship_class_id)"
+        object_name_lists = Array{Array{String,1},1}()
+        for relationship in py"[x._asdict() for x in $relationship_list]"
+            object_name_list = [String(x) for x in split(relationship["object_name_list"], ",")]
+            push!(object_name_lists, object_name_list)
+        end
+        @suppress_err begin
+            @eval begin
+                function $(Symbol(relationship_class_name))(;kwargs...)
+                    result = $(object_name_lists)
+                    object_class_name_list = $(object_class_name_list)
+                    for (key,value) in kwargs
+                        index = findfirst(x -> x == string(key), object_class_name_list)
+                        result = filter(x -> x[index] == value, result)
+                        result = [x[1:end .!= index] for x in result]
+                    end
+                    result
+                end
+                export $(Symbol(relationship_class_name))
+            end
+        end
+    end
+end
+
+function JuMP_object_parameter_out(mapping::PyObject)
+    parameter_list = py"$mapping.parameter_list()"
+    for parameter in py"[x._asdict() for x in $parameter_list]"
+        parameter_name = parameter["name"]
+        count_ = py"$mapping.object_parameter_value_list(parameter_name=$parameter_name).count()"
+        count_ == 0 && continue
+        object_parameter_value_list =
+            py"$mapping.object_parameter_value_list(parameter_name=$parameter_name)"
+        object_parameter_value_dict = Dict{String,Any}()
+        for object_parameter_value in py"[x._asdict() for x in $object_parameter_value_list]"
+            object_name = object_parameter_value["object_name"]
+            json = object_parameter_value["json"]
+            value = object_parameter_value["value"]
+            object_parameter_value_dict[object_name] = Dict{String,Any}(
+                "json" => json,
+                "value" => as_number(value)
+            )
+        end
+        @suppress_err begin
+            @eval begin
+                function $(Symbol(parameter_name))(;t::Int64=1, kwargs...)
+                    length(kwargs) != 1 && return nothing
+                    key, value = kwargs[1]
+                    object_parameter_value_dict = $(object_parameter_value_dict)
+                    object_class_name = string(key)  # NOTE: not in use at the moment
+                    object_name = value
+                    !haskey(object_parameter_value_dict, object_name) && return nothing
+                    result = object_parameter_value_dict[object_name]
+                    result["json"] == nothing && return result["value"]
+                    return result["json"][t]
+                end
+                export $(Symbol(parameter_name))
+            end
+        end
+    end
+end
+
+function JuMP_relationship_parameter_out(mapping::PyObject)
+    parameter_list = py"$mapping.parameter_list()"
+    for parameter in py"[x._asdict() for x in $parameter_list]"
+        parameter_name = parameter["name"]
+        count_ = py"$mapping.relationship_parameter_value_list(parameter_name=$parameter_name).count()"
+        count_ == 0 && continue
+        relationship_parameter_value_list =
+            py"$mapping.relationship_parameter_value_list(parameter_name=$parameter_name)"
+        # Get object_class_name_list from first row in the result
+        object_class_name_list = nothing
+        for relationship_parameter_value in py"[x._asdict() for x in $relationship_parameter_value_list]"
+            object_class_name_list = [
+                String(x) for x in split(relationship_parameter_value["object_class_name_list"], ",")
+            ]
+            break
+        end
+        # Append an increasing integer to object classes that are repeated (eg, commodity1, commodity2)
+        object_class_name_ocurrences = Dict{String,Int64}()
+        for (i, object_class_name) in enumerate(object_class_name_list)
+            n_ocurrences = count(x -> contains(x, object_class_name), object_class_name_list)
+            n_ocurrences == 1 && continue
+            ocurrence = get(object_class_name_ocurrences, object_class_name, 1)
+            object_class_name_list[i] = string(object_class_name, ocurrence)
+            object_class_name_ocurrences[object_class_name] = ocurrence + 1
+        end
+        relationship_parameter_value_dict = Dict{String,Any}()
+        for relationship_parameter_value in py"[x._asdict() for x in $relationship_parameter_value_list]"
+            object_name_list = relationship_parameter_value["object_name_list"]
+            json = relationship_parameter_value["json"]
+            value = relationship_parameter_value["value"]
+            relationship_parameter_value_dict[object_name_list] = Dict{String,Any}(
+                "json" => json,
+                "value" => as_number(value)
+            )
+        end
+        @suppress_err begin
+            @eval begin
+                function $(Symbol(parameter_name))(;t::Int64=1, kwargs...)
+                    relationship_parameter_value_dict = $(relationship_parameter_value_dict)
+                    object_class_name_list = $(object_class_name_list)
+                    kwargs_dict = Dict(kwargs)
+                    object_name_list = Array{String,1}()
+                    for object_class_name in object_class_name_list
+                        if haskey(kwargs_dict, Symbol(object_class_name))
+                            push!(object_name_list, kwargs_dict[Symbol(object_class_name)])
+                            continue
+                        end
+                    end
+                    object_name_list = join(object_name_list, ",")
+                    !haskey(relationship_parameter_value_dict, object_name_list) && return nothing
+                    result = relationship_parameter_value_dict[object_name_list]
+                    result["json"] == nothing && return result["value"]
+                    return result["json"][t]
+                end
+                export $(Symbol(parameter_name))
+            end
+        end
+    end
 end
 
 """
-    JuMP_all_out(mapping::PyObject, update_all_datatypes=true)
+    JuMP_all_out(mapping::PyObject)
 
 Generate and export convenience functions
 named after each object class, relationship class, and parameter in the
@@ -34,10 +183,6 @@ These functions are intended to be called in JuMP programs, as follows:
   - **parameter**: call `z("k", t)` to get the value of the parameter named `"z"` for the object
     named `"k"`, or `Nullable()` if the parameter is not defined.
     If this value is an array in the Spine object, then `z("k", t)` returns position `t` in that array.
-
-If `update_all_datatypes` is `true`, then the method tries to find the julia `Type` that best fits
-all values for every parameter in the database, and converts all values to that `Type`.
-(See `update_all_datatypes!`.)
 
 # Example
 ```julia
@@ -58,124 +203,9 @@ julia> demand(node="Leuven", t=17)
 700
 ```
 """
-function JuMP_all_out(mapping::PyObject, update_all_datatypes=true)
-    object_class_list = py"$mapping.object_class_list()"
-    for object_class in py"[x._asdict() for x in $object_class_list]"
-        object_class_id = object_class["id"]
-        object_class_name = object_class["name"]
-        object_list = py"$mapping.object_list(class_id=$object_class_id)"
-        object_names = py"[x.name for x in $object_list]"
-        @suppress_err begin
-            @eval begin
-                $(Symbol(object_class_name))() = $(object_names)
-                export $(Symbol(object_class_name))
-            end
-        end
-    end
-    relationship_class_list = py"$mapping.wide_relationship_class_list()"
-    for relationship_class in py"[x._asdict() for x in $relationship_class_list]"
-        relationship_class_id = relationship_class["id"]
-        relationship_class_name = relationship_class["name"]
-        object_class_name_list = [Symbol(x) for x in split(relationship_class["object_class_name_list"], ",")]
-        relationship_list = py"$mapping.wide_relationship_list(class_id=$relationship_class_id)"
-        object_name_lists = Array{Array{String,1},1}()
-        for relationship in py"[x._asdict() for x in $relationship_list]"
-            object_name_list = [String(x) for x in split(relationship["object_name_list"], ",")]
-            push!(object_name_lists, object_name_list)
-        end
-        @suppress_err begin
-            @eval begin
-                function $(Symbol(relationship_class_name))(;kwargs...)
-                    result = $(object_name_lists)
-                    object_class_name_list = $(object_class_name_list)
-                    for (key,value) in kwargs
-                        index = findfirst(x -> x == key, object_class_name_list)
-                        result = filter(x -> x[index] == value, result)
-                        result = [x[1:end .!= index] for x in result]
-                    end
-                    result
-                end
-                export $(Symbol(relationship_class_name))
-            end
-        end
-    end
-    parameter_list = py"$mapping.parameter_list()"
-    for parameter in py"[x._asdict() for x in $parameter_list]"
-        parameter_name = parameter["name"]
-        object_parameter_value_list =
-            py"$mapping.object_parameter_value_list(parameter_name=$parameter_name)"
-        object_parameter_value_dict = Dict{String,Any}()
-        for object_parameter_value in py"[x._asdict() for x in $object_parameter_value_list]"
-            object_name = object_parameter_value["object_name"]
-            json = object_parameter_value["json"]
-            value = object_parameter_value["value"]
-            object_parameter_value_dict[object_name] = Dict{String,Any}(
-                "json" => json,
-                "value" => as_number(value)
-            )
-        end
-        relationship_parameter_value_list =
-            py"$mapping.relationship_parameter_value_list(parameter_name=$parameter_name)"
-        object_class_name_list = nothing
-        for relationship_parameter_value in py"[x._asdict() for x in $relationship_parameter_value_list]"
-            object_class_name_list = [
-                String(x) for x in split(relationship_parameter_value["object_class_name_list"], ",")
-            ]
-            break
-        end
-        object_class_name_list == nothing && continue
-        object_class_name_ocurrences = Dict{String,Int64}()
-        for (i, object_class_name) in enumerate(object_class_name_list)
-            n_ocurrences = count(x -> contains(x, object_class_name), object_class_name_list)
-            n_ocurrences == 1 && continue
-            ocurrence = get(object_class_name_ocurrences, object_class_name, 1)
-            object_class_name_list[i] = string(object_class_name, ocurrence)
-            object_class_name_ocurrences[object_class_name] = ocurrence + 1
-        end
-        object_class_name_list
-        relationship_parameter_value_dict = Dict{String,Any}()
-        for relationship_parameter_value in py"[x._asdict() for x in $relationship_parameter_value_list]"
-            object_name_list = relationship_parameter_value["object_name_list"]
-            json = relationship_parameter_value["json"]
-            value = relationship_parameter_value["value"]
-            relationship_parameter_value_dict[object_name_list] = Dict{String,Any}(
-                "json" => json,
-                "value" => as_number(value)
-            )
-        end
-        @suppress_err begin
-            @eval begin
-                function $(Symbol(parameter_name))(;t::Int64=1, kwargs...)
-                    if length(kwargs) == 1
-                        object_parameter_value_dict = $(object_parameter_value_dict)
-                        (key, value) = kwargs[1]
-                        object_class_name = string(key)  # NOTE: not in use at the moment
-                        object_name = value
-                        !haskey(object_parameter_value_dict, object_name) && return nothing
-                        result = object_parameter_value_dict[object_name]
-                        result["json"] == nothing && return result["value"]
-                        return result["json"][t]
-                    else
-                        relationship_parameter_value_dict = $(relationship_parameter_value_dict)
-                        object_class_name_list = $(object_class_name_list)
-                        kwargs_dict = Dict(kwargs)
-                        object_name_list = Array{String,1}()
-                        for object_class_name in object_class_name_list
-                            if haskey(kwargs_dict, Symbol(object_class_name))
-                                push!(object_name_list, kwargs_dict[Symbol(object_class_name)])
-                                continue
-                            end
-                        end
-                        object_name_list = join(object_name_list, ",")
-                        !haskey(relationship_parameter_value_dict, object_name_list) && return nothing
-                        result = relationship_parameter_value_dict[object_name_list]
-                        result["json"] == nothing && return result["value"]
-                        return result["json"][t]
-                    end
-                    nothing
-                end
-                export $(Symbol(parameter_name))
-            end
-        end
-    end
+function JuMP_all_out(mapping::PyObject)
+    JuMP_object_out(mapping)
+    JuMP_relationship_out(mapping)
+    JuMP_object_parameter_out(mapping)
+    JuMP_relationship_parameter_out(mapping)
 end
