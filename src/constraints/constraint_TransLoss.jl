e76de933944165b4b465266708688575ca491192
Steffen Kaminski <steffen.kaminski@kuleuven.be>
2018-08-29 12:07:06 +0200

    fixed TransCap and TransLoss constraint

--- src/constraints/constraint_TransLoss.jl
+++ src/constraints/constraint_TransLoss.jl
@@ -1,8 +1,8 @@
 ### t.b.d.
 function constraint_TransLoss(m::Model, trans)
-        @constraint(m, [ con in connection(), i in node(),j in node(), t=1:number_of_timesteps("timer"); [con,i,j] in generate_ConnectionNodePairs()], #for all simmetric connections
-        + (trans[con,i,j,t])
-                * p_TransLoss(con,i,j) ### to have a look at
+        @constraint(m, [con in connection(), i in node(),j in node(), t=1:number_of_timesteps(time="timer"); [con,i,j] in connection_node_node()], #for all symmetric connections
+        + (trans[connection = con, node1=i, node2=j, t=t])
+                * p_TransLoss(connection=con,node1=i,node2=j) ### to have a look at
         >=
-        - (trans[con,j,i ,t]))
+        - (trans[connection=con, node1=j, node2=i ,t=t]))
 end
