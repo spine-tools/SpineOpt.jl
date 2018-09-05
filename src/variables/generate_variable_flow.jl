019819208be90223f2eae377e9c6f7e0aff51f4b
Maren Ihlemann <maren.ihlemann@kuleuven.be>
2018-09-05 10:18:31 +0200

    style edit

--- src/variables/generate_variable_flow.jl
+++ src/variables/generate_variable_flow.jl
@@ -6,7 +6,7 @@ function generate_variable_flow(m::Model)
             n in node(),
             u in unit(),
             d in direction(),
-            t = 1:number_of_timesteps(time="timer");
+            t = 1:number_of_timesteps(time = "timer");
             [c, n, u, d] in commodity__node__unit__direction()
         ] >= 0
     )
