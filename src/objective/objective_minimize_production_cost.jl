5ae5ebab2d838a4cd612821ffc5e53dd45ba5a2e
Manuel Marin <manuelma@kth.se>
2018-09-12 15:23:28 +0200

    - Change mapping to db_map in Spine.jl - Fix JuMP_all_out to work with last version of spinedatabase_api

--- src/objective/objective_minimize_production_cost.jl
+++ src/objective/objective_minimize_production_cost.jl
@@ -8,8 +8,8 @@ function objective_minimize_production_cost(m::Model, flow)
     production_cost=zero(AffExpr)
     for t=1:number_of_timesteps(time="timer")
         for (c, n, u, d) in commodity__node__unit__direction()
-            if conversion_cost(unit=u, commodity=c) !=nothing
-            production_cost += flow[c, n, u, d, t] * conversion_cost(unit=u, commodity=c)
+            if conversion_cost(unit=u, commodity=c) != nothing
+                production_cost += flow[c, n, u, d, t] * conversion_cost(unit=u, commodity=c, t=t)
             end
         end
     end
