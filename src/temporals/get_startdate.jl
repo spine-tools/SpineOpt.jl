89a0437a15b1e3b0bae31d1c3029ad00e0ad79a1
Manuel Marin <manuelma@kth.se>
2019-04-02 13:19:23 -0400

    Remove obsolete files.

--- src/temporals/get_startdate.jl
+++
@@ -1,7 +0,0 @@
-function start_date(k::Symbol)
-    start_date = Dict()
-    start_date = DateTime(start_datetime()[:temporal_block][k][1],(start_datetime()[:temporal_block][k][2]),
-                                (start_datetime()[:temporal_block][k][3]),(start_datetime()[:temporal_block][k][4]),
-                                    (start_datetime()[:temporal_block][k][5]),    (start_datetime()[:temporal_block][k][6]))
-    return start_date
-end
