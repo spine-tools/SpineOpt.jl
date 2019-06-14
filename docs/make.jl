b71ffaa81231cf9d4a1a57964ec66c0c28390637
Manuel Marin <manuelma@kth.se>
2019-06-14 15:14:08 -0400

    remove dup intersect and fix make_docs

--- docs/make.jl
+++ docs/make.jl
@@ -7,5 +7,5 @@ makedocs(
 deploydocs(
     repo  = "github.com/Spine-project/Spine-Model.git",
     devurl = "dev",
-    versions = ["stable" => "v^", "v#.#", devurl => devurl]
+    versions = ["stable" => "v^", "v#.#", devurl => "dev"]
 )
