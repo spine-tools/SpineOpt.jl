06bf17c4bc2a112c5a88550c979fb806a403d5bf
Manuel Marin <manuelma@kth.se>
2019-06-14 07:37:27 -0400

    understanding `deploydocs`

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,5 @@ using Documenter, SpineModel
 makedocs(sitename = "SpineModel Documentation")
 
 deploydocs(
-    deps  = Deps.pip("mkdocs", "python-markdown-math"),
-    repo  = "github.com/Spine-project/Spine-Model.git",
-    julia = "1.0"
+    repo  = "github.com/Spine-project/Spine-Model.git"
 )
