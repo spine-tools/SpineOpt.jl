41accd644a2392076cddea522b2157e04d792d91
Topi Rasku <topi.rasku@vtt.fi>
2019-08-06 15:20:12 +0300

    Changes to accommodate *rolling* optimization.

--- examples/test_systemA4.jl
+++ examples/test_systemA4.jl
@@ -32,10 +32,11 @@ end
 function constraint_stor_cyclic(m::Model)
     @fetch stor_state = m.ext[:variables]
     constr_dict = m.ext[:constraints][:stor_cyclic] = Dict()
-    for (stor, c) in storage__commodity()
+    stor_start = [first(stor_state_indices(storage=stor, commodity=c)) for (stor, c) in storage__commodity()]
+    for (stor, c, t_first) in stor_start
         constr_dict[stor, c] = @constraint(
             m,
-            stor_state[stor, c, time_slice()[1]]
+            stor_state[stor, c, t_first]
             ==
             stor_state[stor, c, time_slice()[end]]
         )
