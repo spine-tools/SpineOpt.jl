1f40059eec98a13422ac59a69e7dfbda8552b4d4
Manuel Marin <manuelma@kth.se>
2019-11-07 13:49:04 +0100

    Rearrange arguments in RelationshipClass constructor
    SpineInterface has switched the order of the arguments so this is
    just needed.

--- src/temporals/generate_time_slice_relationships.jl
+++ src/temporals/generate_time_slice_relationships.jl
@@ -146,9 +146,9 @@ function generate_time_slice_relationships()
     t_in_t_excl_list = [(t_short=t1, t_long=t2) for (t1, t2) in t_in_t_list if t1 != t2]
     t_overlaps_t_excl_list = [(t1, t2) for (t1, t2) in t_overlaps_t_list if t1 != t2]
     # Create function-like objects
-    t_before_t = RelationshipClass(:t_before_t, NamedTuple(), (:t_before, :t_after), t_before_t_list, [])
-    t_in_t = RelationshipClass(:t_in_t, NamedTuple(), (:t_short, :t_long), t_in_t_list, [])
-    t_in_t_excl = RelationshipClass(:t_in_t_excl, NamedTuple(), (:t_short, :t_long), t_in_t_excl_list, [])
+    t_before_t = RelationshipClass(:t_before_t, (:t_before, :t_after), NamedTuple(), t_before_t_list, [])
+    t_in_t = RelationshipClass(:t_in_t, (:t_short, :t_long), NamedTuple(), t_in_t_list, [])
+    t_in_t_excl = RelationshipClass(:t_in_t_excl, (:t_short, :t_long), NamedTuple(), t_in_t_excl_list, [])
     t_overlaps_t = TOverlapsTRelationshipClass(t_overlaps_t_list)
     t_overlaps_t_excl = TOverlapsTExclRelationshipClass(t_overlaps_t_excl_list)
     # Export the function-like objects
