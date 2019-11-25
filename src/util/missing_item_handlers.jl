a872a9c85ce309b07633ec92539b292db94e1633
Topi Rasku <topi.rasku@vtt.fi>
2019-11-25 14:42:21 +0200

    Default value for trans delay

--- src/util/missing_item_handlers.jl
+++ src/util/missing_item_handlers.jl
@@ -154,6 +154,7 @@ const parameters = [
     (:reoptimization_frequency, 0),
     (:rolling_window_duration, 0),
     (:fuel_cost, nothing),
+    (:trans_delay, Second(0)),
 ]
 for name in [object_classes; relationship_classes]
     quoted_name = Expr(:quote, name)
