9a58ba4ab8dd4cf25793b14e8fdc91216d16efc6
Manuel Marin <manuelma@kth.se>
2020-03-31 09:14:41 -0400

    Get rid of direction in a different way
    Turns out there's different options for doing this. Commit c396aa92
    explicitly introduced connection__node__direction and
    unit__node__direction, whereas here we just extend `unit__from_node`
    and `connection__from_node` with the appropriate `direction` object.
    This needed to change `SpineInterface.RelationshipClass` so all the
    fields can be modified, but it's not a big deal. The code now looks
    simpler and that's a good sign.

--- src/util/generate_special_handlers.jl
+++ src/util/generate_special_handlers.jl
@@ -19,8 +19,6 @@
 
 function generate_special_handlers()
     generate_direction()
-    generate_unit__node__direction()
-    generate_connection__node__direction()
     generate_variable_indices()
 end
 
@@ -29,68 +27,37 @@ function generate_direction()
     from_node = Object(:from_node, 1)
     to_node = Object(:to_node, 2)
     direction = ObjectClass(:direction, [from_node, to_node], Dict())
+    directions_by_class = Dict(
+        unit__from_node => from_node,
+        unit__to_node => to_node,
+        connection__from_node => from_node,
+        connection__to_node => to_node,
+    )
+    for cls in keys(directions_by_class)
+        push!(cls.object_class_names, :direction)
+    end
+    for (cls, d) in directions_by_class
+        map!(rel -> (; rel..., direction=d), cls.relationships, cls.relationships)
+        key_map = Dict(rel => (rel..., d) for rel in keys(cls.parameter_values))
+        for (key, new_key) in key_map
+            cls.parameter_values[new_key] = pop!(cls.parameter_values, key)
+        end
+    end
     @eval begin
         direction = $direction
         export direction
     end
 end
 
-function generate_unit__node__direction()
-    from_node = direction(:from_node)
-    to_node = direction(:to_node)
-    relationships = [(unit=u, node=n, direction=from_node) for (u, n) in unit__from_node.relationships]
-    append!(relationships, [(unit=u, node=n, direction=to_node) for (u, n) in unit__to_node.relationships])
-    parameter_values = Dict((u, n, from_node) => val for ((u, n), val) in unit__from_node.parameter_values)
-    merge!(parameter_values, Dict((u, n, to_node) => val for ((u, n), val) in unit__to_node.parameter_values))
-    unit__node__direction = RelationshipClass(
-        :unit__node__direction, (:unit, :node, :direction), relationships, parameter_values
-    )
-    @eval begin
-        unit__node__direction = $unit__node__direction
-        export unit__node__direction
-    end
-    parameter_names = Set(keys(first(values(unit__from_node.parameter_values))))
-    union!(parameter_names, keys(first(values(unit__to_node.parameter_values))))
-    for parameter_name in parameter_names
-        parameter = getfield(SpineModel, parameter_name)
-        filter!(class -> !(class in (unit__from_node, unit__to_node)), parameter.classes)
-        push!(parameter.classes, unit__node__direction)
-    end
-end 
-
-function generate_connection__node__direction()
-    from_node = direction(:from_node)
-    to_node = direction(:to_node)
-    relationships = [(connection=conn, node=n, direction=from_node) for (conn, n) in connection__from_node.relationships]
-    append!(relationships, [(connection=conn, node=n, direction=to_node) for (conn, n) in connection__to_node.relationships])
-    parameter_values = Dict((conn, n, from_node) => val for ((conn, n), val) in connection__from_node.parameter_values)
-    merge!(parameter_values, Dict((conn, n, to_node) => val for ((conn, n), val) in connection__to_node.parameter_values))
-    connection__node__direction = RelationshipClass(
-        :connection__node__direction, (:connection, :node, :direction), relationships, parameter_values
-    )
-    @eval begin
-        connection__node__direction = $connection__node__direction
-        export connection__node__direction
-    end
-    parameter_names = Set(keys(first(values(connection__from_node.parameter_values))))
-    union!(parameter_names, keys(first(values(connection__to_node.parameter_values))))
-    for parameter_name in parameter_names
-        parameter = getfield(SpineModel, parameter_name)
-        filter!(class -> !(class in (connection__from_node, connection__to_node)), parameter.classes)
-        push!(parameter.classes, connection__node__direction)
-    end
-end
-
-
 function generate_variable_indices()
     unit_flow_indices = unique(
         (unit=u, node=n, direction=d, temporal_block=tb)
-        for (u, n, d) in unit__node__direction()
+        for (u, n, d) in Iterators.flatten((unit__from_node(), unit__to_node()))
         for tb in node__temporal_block(node=n)
     )
     connection_flow_indices = unique(
         (connection=conn, node=n, direction=d, temporal_block=tb)
-        for (conn, n, d) in connection__node__direction()
+        for (conn, n, d) in Iterators.flatten((connection__from_node(), connection__to_node()))
         for tb in node__temporal_block(node=n)
     )
     node_state_indices = unique(
@@ -99,13 +66,13 @@ function generate_variable_indices()
         for tb in node__temporal_block(node=n)
     )
     unit_flow_indices_rc = RelationshipClass(
-        :unit_flow_indices_rc, (:unit, :node, :direction, :temporal_block), unit_flow_indices
+        :unit_flow_indices_rc, [:unit, :node, :direction, :temporal_block], unit_flow_indices
     )
     connection_flow_indices_rc = RelationshipClass(
-        :connection_flow_indices_rc, (:connection, :node, :direction, :temporal_block), connection_flow_indices
+        :connection_flow_indices_rc, [:connection, :node, :direction, :temporal_block], connection_flow_indices
     )
     node_state_indices_rc = RelationshipClass(
-        :node_state_indices_rc, (:node, :temporal_block), node_state_indices
+        :node_state_indices_rc, [:node, :temporal_block], node_state_indices
     )
     @eval begin
         unit_flow_indices_rc = $unit_flow_indices_rc
