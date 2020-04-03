6d5b99e760213e8c57c580b3f2dabb73bfef4dd7
Manuel Marin <manuelma@kth.se>
2020-04-03 15:50:09 -0400

    Remove one loop from constraint_connection_flow_capacity
    I don't think we can afford this double loop for large datasets
    at the moment, even though it feels like the correct way to do it.
    The default value of `connection_availability_factor` is 1.0 anyways,
    which essentially means the removed loop is redundant?

--- src/constraints/constraint_connection_flow_capacity.jl
+++ src/constraints/constraint_connection_flow_capacity.jl
@@ -28,17 +28,15 @@ function add_constraint_connection_flow_capacity!(m::Model)
     @fetch connection_flow = m.ext[:variables]
     cons = m.ext[:constraints][:connection_flow_capacity] = Dict()
     for (conn, n, d) in indices(connection_capacity)
-        for (conn,) in indices(connection_availability_factor; connection=conn)
-            for (conn, n, d, t) in connection_flow_indices(connection=conn, node=n, direction=d)
-                cons[conn, n, t] = @constraint(
-                    m,
-                    connection_flow[conn, n, d, t]
-                    <=
-                    + connection_capacity[(connection=conn, node=n, direction=d, t=t)]
-                    * connection_availability_factor[(connection=conn, t=t)]
-                    * connection_conv_cap_to_flow[(connection=conn, node=n, direction=d, t=t)]
-                )
-            end
+        for (conn, n, d, t) in connection_flow_indices(connection=conn, node=n, direction=d)
+            cons[conn, n, t] = @constraint(
+                m,
+                connection_flow[conn, n, d, t]
+                <=
+                + connection_capacity[(connection=conn, node=n, direction=d, t=t)]
+                * connection_availability_factor[(connection=conn, t=t)]
+                * connection_conv_cap_to_flow[(connection=conn, node=n, direction=d, t=t)]
+            )
         end
     end
 end
