281053514b811b0b1789e8c459bda5c10d8cecc3
Rasku Topi <topi.rasku@vtt.fi>
2020-05-06 07:52:57 +0300

    #141 `constraint_unit_state_transition` stochastic path indexing.

--- src/constraints/constraint_unit_state_transition.jl
+++ src/constraints/constraint_unit_state_transition.jl
@@ -17,6 +17,38 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #############################################################################
 
+"""
+    constraint_unit_state_transition_indices()
+
+Forms the stochastic index set for the `:unit_state_transition` constraint.
+Uses stochastic path indices due to potentially different stochastic scenarios
+between `t_after` and `t_before`.
+"""
+function constraint_unit_state_transition_indices()
+    unit_state_transition_indices = []
+    for (u, n) in unit__structure_node_rc()
+        for t_after in time_slice(temporal_block=node__temporal_block(node=n))
+            # `units_on` on `t_after`
+            active_scenarios = units_on_indices_rc(unit=u, t=t_after, _compact=true)
+            # `units_on` on `t_before`
+            t_before = first(t_before_t(t_after=t_after))
+            append!(
+                active_scenarios,
+                units_on_indices_rc(unit=u, t=t_before, _compact=true)
+            )
+            # Find stochastic paths for `active_scenarios`
+            unique!(active_scenarios)
+            for path in active_stochastic_paths(full_stochastic_paths, active_scenarios)
+                push!(
+                    unit_state_transition_indices,
+                    (unit=u, stochastic_path=path, t_before=t_before, t_after=t_after)
+                )
+            end
+        end
+    end
+    return unique!(unit_state_transition_indices)
+end
+
 
 """
     add_constraint_unit_state_transition!(m::Model)
@@ -27,16 +59,19 @@ and `units_shut_down`.
 function add_constraint_unit_state_transition!(m::Model)
     @fetch units_on, units_started_up, units_shut_down = m.ext[:variables]
     cons = m.ext[:constraints][:unit_state_transition] = Dict()
-    for (u, t_after) in units_on_indices()
-        for (u, t_before) in units_on_indices(unit=u, t=t_before_t(t_after=t_after))
-            cons[u, t_before, t_after] = @constraint(
-                m,
-                + units_on[u, t_after]
-                ==
-                + units_on[u, t_before]
-                + units_started_up[u, t_after]
-                - units_shut_down[u, t_after]
+    for (u, stochastic_path, t_before, t_after) in constraint_unit_state_transition_indices()
+        cons[u, stochastic_path, t_before, t_after] = @constraint(
+            m,
+            reduce(+, get(units_on, (u, s, t_after), 0) for s in stochastic_path; init=0)
+            ==
+            reduce(
+                +,
+                get(units_on, (u, s, t_before), 0)
+                + get(units_started_up, (u, s, t_after), 0)
+                - get(units_shut_down, (u, s, t_after), 0)
+                for s in stochastic_path;
+                init=0
             )
-        end
+        )
     end
 end
