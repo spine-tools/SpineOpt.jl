a241b2ec90968b87b7e4f50575a283a6a68d2d71
Jody Dillon <jody.dillon@energyreform.ie>
2020-05-08 17:15:02 +0200

    Timeslice tool included - see https://gitlab.vtt.fi/spine/model/-/issues/175

---
+++ src/timeslicetool/preprocess_data_structure.jl
@@ -0,0 +1,233 @@
+#############################################################################
+# Copyright (C) 2017 - 2018  Spine Project
+#
+# This file is part of Spine Model.
+#
+# Spine Model is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Spine Model is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#############################################################################
+
+function preprocess_data_structure()
+    generate_blocks()
+    generate_resources()
+    window__static_slice = generate_distributions()
+end
+
+""" The representative periods model decomposes a distribution into a number of operatig point blocks
+    as specified by representative_blocks(representative_period=rp). Here we create a new object class
+    called block and create an object for each block """
+
+function generate_blocks()
+    rp=first(representative_period())
+    block = ObjectClass(:block, [])
+    # Create block objects named B1, B2... BN etc.
+    block_objects = ObjectLike[]
+    for b in 1:representative_blocks(representative_period=rp)
+        push!(block_objects, Object(Symbol(string("B", Int(b)))))
+    end
+    add_objects!(block, block_objects)
+
+    @eval begin
+        block = $block
+    end
+end
+
+
+""" The representative period model selects a number of days that best represent the distribution of various time series
+    For now, that's either a `demand` time series or a `unit_availability_factor` time series or a `unit_capacity` time series
+    In the model we generalise these into a `resource`. We define this as a new object_class and copy into it
+    and `nodes` or `units` that have a `node__representative_period` or `unit__representative_period` relationship respectively """
+
+
+function generate_resources()
+    resource = ObjectClass(:resource, [])
+    for (u, rp) in unit__representative_period()
+        r = Object(Symbol(u.name))
+        add_object!(resource, r)
+        # copy the timeseries data to a new unified resource object
+        resource.parameter_values[r] = Dict()
+        resource.parameter_values[r][:resource_availability] = unit.parameter_values[u][:unit_availability_factor]
+        # each resource time series can have a weight defined by representative_period_weight(unit=u, representative_period=rp). It should have a default of 1, but we check just in case
+        if haskey(unit__representative_period.parameter_values[(u, rp)], :representative_period_weight)
+            resource.parameter_values[r][:representative_period_weight] = unit__representative_period.parameter_values[(u, rp)][:representative_period_weight]
+        else
+            resource.parameter_values[r][:representative_period_weight] = SpineInterface.ScalarParameterValue(1.0)
+        end
+    end
+    for (n, rp) in node__representative_period()
+        r = Object(Symbol(n.name))
+        add_object!(resource, r)
+        # copy the timeseries data to a new unified resource object
+        resource.parameter_values[r] = Dict()
+        resource.parameter_values[r][:resource_availability] = node.parameter_values[n][:demand]
+        # each resource time series can have a weight defined by representative_period_weight(node=n, representative_period=rp). It should have a default of 1, but we check just in case
+        if haskey(node__representative_period.parameter_values[(n, rp)], :representative_period_weight)
+            resource.parameter_values[r][:representative_period_weight] = node__representative_period.parameter_values[(n, rp)][:representative_period_weight]
+        else
+            resource.parameter_values[r][:representative_period_weight] = SpineInterface.ScalarParameterValue(1.0)
+        end
+    end
+    for (u, n, rp) in unit__node__representative_period()
+        r = Object(Symbol(string(u.name, "_", n.name)))
+        add_object!(resource, r)
+        # copy the timeseries data to a new unified resource object
+        resource.parameter_values[r] = Dict()
+        resource.parameter_values[r][:resource_availability] = unit__node.parameter_values[(u, n)][:unit_capacity]
+        # each resource time series can have a weight defined by representative_period_weight(node=n, representative_period=rp). It should have a default of 1, but we check just in case
+        if haskey(unit__node__representative_period.parameter_values[(u, n, rp)], :representative_period_weight)
+            resource.parameter_values[r][:representative_period_weight] = unit__node__representative_period.parameter_values[(u, n, rp)][:representative_period_weight]
+        else
+            resource.parameter_values[r][:representative_period_weight] = SpineInterface.ScalarParameterValue(1.0)
+        end
+    end
+
+    # add the resource_availability parameter to the resource object class
+    push!(representative_period_weight.classes, resource)
+    resource_availability = Parameter(:resource_availability, [resource])
+
+    @eval begin
+        resource = $resource
+        resource_availability = $resource_availability
+    end
+end
+
+""" Generate the distribution for each time series as defined by:
+     - Number of operating point segments (blocks):  """
+
+function generate_distributions()
+
+    rp=first(representative_period())
+    n_periods = representative_periods(representative_period=rp)
+
+    window = ObjectClass(:window, [])
+    static_slice = ObjectClass(:static_slice, [])
+
+    @eval begin
+        window = $window
+        static_slice = $static_slice
+    end
+
+    ts_vals = Dict()
+    ts_vals_window = Dict()
+
+    res_dist_window = Dict()
+    res_dist_horizon = Dict()
+    bin_interval = Dict()
+    ts_vals = Dict()
+    ts_max = Dict()
+    ts_min = Dict()
+    window__static_slice = Dict()
+    ss_ts = Dict()
+
+    for r in resource()
+        res_dist_horizon[r] = zeros(size(block(),1))
+        ts_max[r] = resource_availability(resource=r, t=first(time_slice()))
+        ts_min[r] = ts_max[r]
+    end
+
+    i_win = 1
+    while true
+        w = Object(Symbol(string("W", Int(i_win))))
+
+        add_object!(window, w)
+
+        window__static_slice[w] = []
+
+        for t in time_slice()
+            ss = Object(Symbol(string(t)))
+            add_object!(static_slice, ss)
+            push!(window__static_slice[w], ss)
+            ss_ts[t] = ss
+        end
+
+        for r in resource()
+            res_dist_window[r, w] = zeros(length(block()))
+            for t in time_slice()
+                ts_vals[r, ss_ts[t]] = resource_availability(resource=r, t=t)
+                (ts_vals[r, ss_ts[t]] == nothing) && (ts_vals[r, ss_ts[t]] = 0)
+                (ts_vals[r, ss_ts[t]] > ts_max[r]) && (ts_max[r] = ts_vals[r, ss_ts[t]])
+                (ts_vals[r, ss_ts[t]] < ts_min[r]) && (ts_min[r] = ts_vals[r, ss_ts[t]])
+            end
+        end
+        SpineModel.roll_temporal_structure() || break
+        i_win += 1
+    end
+
+    io = open("ts_vals.csv", "w")
+    print(io, "window,ts")
+    for r in resource()
+        print(io, string(",", r))
+    end
+    print(io, "\n")
+    for w in window()
+        for ss in window__static_slice[w]
+            print(io, string(w, ",", ss))
+            for r in resource()
+                print(io, string(",", ts_vals[r, ss]))
+            end
+            print(io, "\n")
+        end
+    end
+    close(io)
+
+    window_time_interval = 100/length(window__static_slice[first(window())])
+    horizon_time_interval = window_time_interval/length(window())
+    for r in resource()
+        bin_interval = (ts_max[r] - ts_min[r]) / length(block())
+        for w in window()
+            for ss in window__static_slice[w]
+                lowestb = floor(Int, (ts_max[r] - ts_vals[r, ss]) / bin_interval)
+                (lowestb == 0) && (lowestb = 1)
+                for b in lowestb:length(block())
+                    res_dist_window[r, w][b] += window_time_interval
+                    res_dist_horizon[r][b] += horizon_time_interval
+                end
+            end
+        end
+    end
+
+    res_blk_parameter_values = Dict(
+        (r, b) => Dict(:resource_distribution => SpineInterface.ScalarParameterValue(res_dist_horizon[r][parse(Int,string(b.name)[2:end])]))
+        for r in resource() for b in block()
+    )
+
+    resource__block = RelationshipClass(
+        :resource__block,
+        [:resource, :block],
+        [(resource=r, block=b) for r in resource() for b in block()],
+        res_blk_parameter_values
+    )
+
+    res_blk_wdw_parameter_values = Dict(
+        (r, b, w) => Dict(:resource_distribution_window => SpineInterface.ScalarParameterValue(res_dist_window[r, w][parse(Int,string(b.name)[2:end])]))
+        for r in resource() for b in block() for w in window()
+    )
+
+    resource__block__window = RelationshipClass(
+        :resource__block__window,
+        [:resource, :block, :window],
+        [(resource=r, block=b, window=r) for r in resource() for b in block() for w in window()],
+        res_blk_wdw_parameter_values
+    )
+
+    resource_distribution = Parameter(:resource_distribution, [resource__block])
+    resource_distribution_window = Parameter(:resource_distribution_window, [resource__block__window])
+
+    @eval begin
+        resource__block = $resource__block
+        resource__block__window = $resource__block__window
+        resource_distribution = $resource_distribution
+        resource_distribution_window = $resource_distribution_window
+    end
+    window__static_slice
+end
---
+++ src/timeslicetool/timeslicetool.jl
@@ -0,0 +1,262 @@
+#############################################################################
+# Copyright (C) 2017 - 2018  Spine Project
+#
+# This file is part of Spine Model.
+#
+# Spine Model is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Spine Model is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#############################################################################
+"""
+    run_spinemodel(url; <keyword arguments>)
+
+Run the Spine model from `url` and write report to the same `url`.
+Keyword arguments have the same purpose as for [`run_spinemodel`](@ref).
+"""
+function run_spinemodel(
+        url::String;
+        with_optimizer=optimizer_with_attributes(Cbc.Optimizer, "logLevel" => 0),
+        cleanup=true,
+        add_constraints=m -> nothing,
+        update_constraints=m -> nothing,
+        log_level=3)
+    run_spinemodel(
+        url,
+        url;
+        with_optimizer=with_optimizer,
+        cleanup=cleanup,
+        add_constraints=add_constraints,
+        update_constraints=update_constraints,
+        log_level=log_level
+    )
+end
+
+
+function generate_temporal_structure()
+    generate_current_window()
+    generate_time_slice()
+    generate_time_slice_relationships()
+end
+
+
+"""
+    run_spinemodel(url_in, url_out; <keyword arguments>)
+
+Run the Spine model from `url_in` and write report to `url_out`.
+At least `url_in` must point to valid Spine database.
+A new Spine database is created at `url_out` if it doesn't exist.
+
+# Keyword arguments
+
+**`with_optimizer=with_optimizer(Cbc.Optimizer, logLevel=0)`** is the optimizer factory for building the JuMP model.
+
+**`cleanup=true`** tells [`run_spinemodel`](@ref) whether or not convenience functors should be
+set to `nothing` after completion.
+
+**`add_constraints=m -> nothing`** is called with the `Model` object in the first optimization window, and allows adding user contraints.
+
+**`update_constraints=m -> nothing`** is called in windows 2 to the last, and allows updating contraints added by `add_constraints`.
+
+**`log_level=3`** is the log level.
+"""
+function run_spinemodel(
+        url_in::String,
+        url_out::String;
+        with_optimizer=optimizer_with_attributes(Cbc.Optimizer, "logLevel" => 0, "ratioGap" => 0.01),
+        cleanup=true,
+        add_constraints=m -> nothing,
+        update_constraints=m -> nothing,
+        log_level=3)
+    level2 = log_level >= 2
+    @log true "Running Spine Model for $(url_in)..."
+    @logtime level2 "Initializing data structure from db..." begin
+        using_spinedb(url_in, @__MODULE__; upgrade=true)
+        generate_missing_items()
+    end
+    @logtime level2 "Preprocessing data structure..." preprocess_data_structure(log_level)
+    rerun_spinemodel(
+        url_out;
+        with_optimizer=with_optimizer,
+        cleanup=cleanup,
+        add_constraints=add_constraints,
+        update_constraints=update_constraints,
+        log_level=log_level
+    )
+end
+
+function rerun_spinemodel(
+        url_out::String;
+        with_optimizer=optimizer_with_attributes(Cbc.Optimizer, "logLevel" => 0, "ratioGap" => 0.01),
+        cleanup=true,
+        add_constraints=m -> nothing,
+        update_constraints=m -> nothing,
+        log_level=3)
+
+    level0 = log_level >= 0
+    level1 = log_level >= 1
+    level2 = log_level >= 2
+    level3 = log_level >= 3
+    results = Dict()
+    @logtime level2 "Creating temporal structure..." generate_temporal_structure()
+    @log level1 "Window 1: $current_window"
+    @logtime level2 "Initializing model..." begin
+        m = Model(with_optimizer)
+        println()
+        m.ext[:variables] = Dict{Symbol,Dict}()
+        m.ext[:variables_lb] = Dict{Symbol,Any}()
+        m.ext[:variables_ub] = Dict{Symbol,Any}()
+        m.ext[:values] = Dict{Symbol,Dict}()
+        m.ext[:constraints] = Dict{Symbol,Dict}()
+        @logtime level3  "creating variables" create_variables!(m)
+        @logtime level3  "handle fix variables" fix_variables!(m)
+        @logtime level3  "objective function" set_objective!(m)
+    end
+    @logtime level2 "Adding constraints...\n" begin
+        @logtime level3 "- [constraint_unit_constraint]" add_constraint_unit_constraint!(m)
+        @logtime level3 "- [constraint_nodal_balance]" add_constraint_nodal_balance!(m)
+        @logtime level3 "- [constraint_group_balance]" add_constraint_group_balance!(m)
+        @logtime level3 "- [constraint_connection_flow_ptdf]" add_constraint_connection_flow_ptdf!(m)
+        @logtime level3 "- [constraint_connection_flow_lodf]" add_constraint_connection_flow_lodf!(m)
+        @logtime level3 "- [constraint_unit_flow_capacity]" add_constraint_unit_flow_capacity!(m)
+        @logtime level3 "- [constraint_operating_point_bounds]" add_constraint_operating_point_bounds!(m)
+        @logtime level3 "- [constraint_operating_point_sum]" add_constraint_operating_point_sum!(m)
+        @logtime level3 "- [constraint_fix_ratio_out_in_unit_flow]" add_constraint_fix_ratio_out_in_unit_flow!(m)
+        @logtime level3 "- [constraint_max_ratio_out_in_unit_flow]" add_constraint_max_ratio_out_in_unit_flow!(m)
+        @logtime level3 "- [constraint_min_ratio_out_in_unit_flow]" add_constraint_min_ratio_out_in_unit_flow!(m)
+        @logtime level3 "- [constraint_fix_ratio_out_out_unit_flow]" add_constraint_fix_ratio_out_out_unit_flow!(m)
+        @logtime level3 "- [constraint_max_ratio_out_out_unit_flow]" add_constraint_max_ratio_out_out_unit_flow!(m)
+        @logtime level3 "- [constraint_fix_ratio_in_in_unit_flow]" add_constraint_fix_ratio_in_in_unit_flow!(m)
+        @logtime level3 "- [constraint_max_ratio_in_in_unit_flow]" add_constraint_max_ratio_in_in_unit_flow!(m)
+        @logtime level3 "- [constraint_fix_ratio_in_out_unit_flow]" add_constraint_fix_ratio_in_out_unit_flow!(m)
+        @logtime level3 "- [constraint_max_ratio_in_out_unit_flow]" add_constraint_max_ratio_in_out_unit_flow!(m)
+        @logtime level3 "- [constraint_min_ratio_in_out_unit_flow]" add_constraint_min_ratio_in_out_unit_flow!(m)
+        @logtime level3 "- [constraint_fix_ratio_out_in_connection_flow]" add_constraint_fix_ratio_out_in_connection_flow!(m)
+        @logtime level3 "- [constraint_max_ratio_out_in_connection_flow]" add_constraint_max_ratio_out_in_connection_flow!(m)
+        @logtime level3 "- [constraint_min_ratio_out_in_connection_flow]" add_constraint_min_ratio_out_in_connection_flow!(m)
+        @logtime level3 "- [constraint_connection_flow_capacity]" add_constraint_connection_flow_capacity!(m)
+        @logtime level3 "- [constraint_node_state_capacity]" add_constraint_node_state_capacity!(m)
+        @logtime level3 "- [constraint_max_cum_in_unit_flow_bound]" add_constraint_max_cum_in_unit_flow_bound!(m)
+        @logtime level3 "- [constraint_units_on]" add_constraint_units_on!(m)
+        @logtime level3 "- [constraint_units_available]" add_constraint_units_available!(m)
+        @logtime level3 "- [constraint_minimum_operating_point]" add_constraint_minimum_operating_point!(m)
+        @logtime level3 "- [constraint_min_down_time]" add_constraint_min_down_time!(m)
+        @logtime level3 "- [constraint_min_up_time]" add_constraint_min_up_time!(m)
+        @logtime level3 "- [constraint_unit_state_transition]" add_constraint_unit_state_transition!(m)
+        @logtime level3 "- [constraint_user]" add_constraints(m)
+        @logtime level3 "- [setting constraint names]" name_constraints!(m)
+    end
+    k = 2
+
+    while optimize_model!(m)
+        @log level1 "Optimal solution found, objective function value: $(objective_value(m))"
+        @logtime level2 "Saving results..." begin
+            postprocess_results!(m)
+            save_values!(m)
+            save_results!(results, m)
+        end
+        roll_temporal_structure() || break
+        @log level1 "Window $k: $current_window"
+        @logtime level2 "Updating model..." begin
+            update_variables!(m)
+            fix_variables!(m)
+            update_varying_objective!(m)
+        end
+        @logtime level2 "Updating varying constraints..." update_varying_constraints!(m)
+        @logtime level2 "Updating user constraints..." update_constraints(m)
+        k += 1
+    end
+     @logtime level2 "Writing report..." write_report(results, url_out)
+    # TODO: cleanup && notusing_spinedb(url_in, @__MODULE__)
+    m
+end
+
+function optimize_model!(m::Model)
+    # TODO: perhaps add the option to write the mps for diagnostics as follows
+
+    # write_mps_file(model=first(model())) == :write_mps_always && write_to_file(m, "model_diagnostics.mps")
+    # NOTE: The above results in a lot of Warning: Variable connection_flow[...] is mentioned in BOUNDS,
+    # but is not mentioned in the COLUMNS section. We are ignoring it.
+    optimize!(m)
+    if termination_status(m) == MOI.OPTIMAL
+        true
+    else
+        @log true "Unable to find solution (reason: $(termination_status(m)))"
+        # the below needs the parameter write_mps_file - we can uncomment when we update the template perhaps?
+        # write_mps_file(model=first(model())) in (:write_mps_on_no_solve, :write_mps_always) && write_to_file(m, "model_diagnostics.mps")
+        false
+    end
+end
+
+"""
+    save_results!(results, m)
+
+Update `results` with results from `m`.
+"""
+function save_results!(results, m)
+    for out in output()
+        value = get(m.ext[:values], out.name, nothing)
+        if value === nothing
+            @warn "can't find results for '$(out.name)'"
+            continue
+        end
+        value_ = Dict{NamedTuple,Number}((; k..., t=start(k.t)) => v for (k, v) in value)
+        existing = get!(results, out.name, Dict{NamedTuple,Number}())
+        merge!(existing, value_)
+    end
+end
+
+function write_report(results, default_url)
+    reports = Dict()
+    for (rpt, out) in report__output()
+        value = get(results, out.name, nothing)
+        if value === nothing
+            continue
+        end
+        url = output_db_url(report=rpt, _strict=false)
+        url === nothing && (url = default_url)
+        url_reports = get!(reports, url, Dict())
+        report = get!(url_reports, rpt.name, Dict{Symbol,Dict{NamedTuple,TimeSeries}}())
+        d = report[out.name] = Dict{NamedTuple,TimeSeries}()
+        for (k, v) in pulldims(value, :t)
+            inds = first.(v)
+            vals = last.(v)
+            d[k] = TimeSeries(inds, vals, false, false)
+        end
+    end
+    for (url, url_reports) in reports
+        for (rpt_name, output_params) in url_reports
+            write_parameters(output_params, url; report=string(rpt_name))
+        end
+    end
+end
+
+"""
+    pulldims(input, dims...)
+
+An equivalent dictionary where the given dimensions are pulled from the key to the value.
+"""
+function pulldims(input::Dict{K,V}, dims::Symbol...) where {K<:NamedTuple,V}
+    output = Dict()
+    for (key, value) in sort(input)
+        output_key = (; (k => v for (k, v) in pairs(key) if !(k in dims))...)
+        output_value = ((key[dim] for dim in dims)..., value)
+        push!(get!(output, output_key, []), output_value)
+    end
+    output
+end
+
+"""
+    formulation(d::Dict)
+
+An equivalent dictionary where `JuMP.ConstraintRef` values are replaced by their `String` formulation.
+"""
+formulation(d::Dict{K,JuMP.ConstraintRef}) where {K} = Dict{K,Any}(k => sprint(show, v) for (k, v) in d)
