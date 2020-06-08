1838cfa477ff48b596cfa5244035781b03a31741
Jody Dillon <jody.dillon@energyreform.ie>
2020-06-08 20:41:25 +0200

    investments WIP

--- src/variables/variable_units_invested_available.jl
+++ src/variables/variable_units_invested_available.jl
@@ -42,7 +42,7 @@ function add_variable_units_invested_available!(m::Model)
     	:units_invested_available, units_invested_available_indices;
     	lb=x -> 0,
     	int=units_invested_available_int,
-    	fix_value=x -> fix_units_invested(unit=x.unit, t=x.t, _strict=false)
+    	fix_value=x -> fix_units_invested_available(unit=x.unit, t=x.t, _strict=false)
     )
 end
 
