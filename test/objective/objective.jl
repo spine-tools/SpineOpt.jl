55c5586fd8dc93f27645035e2eadd5ce9d3e6478
Manuel Marin <manuelma@kth.se>
2020-06-23 21:09:22 -0400

    Add DelimitedFiles as test dependency

--- test/objective/objective.jl
+++ test/objective/objective.jl
@@ -232,7 +232,6 @@
         scenarios = (stochastic_scenario(:parent), stochastic_scenario(:child))
         time_slices = time_slice(temporal_block=temporal_block(:hourly))
         observed_obj = objective_function(m)
-        _dismember_function(observed_obj)
         expected_obj = connection_flow_cost * sum(connection_flow[(key..., s, t)...] for (s, t) in zip(scenarios, time_slices))
         @test observed_obj == expected_obj
     end
