104e201d6dd0415d539ff6e402c1eb38c8b1b815
Rasku Topi <topi.rasku@vtt.fi>
2020-07-13 09:42:45 +0300

    #141 Revisions to `constraint_units_available` for stochastic parameter implementation.

--- src/constraints/constraint_units_available.jl
+++ src/constraints/constraint_units_available.jl
@@ -29,8 +29,9 @@ function add_constraint_units_available!(m::Model)
             m,
             + units_available[u, s, t]
             ==
-            + ( 
-                + number_of_units[(unit=u, t=t)]
+            + unit_availability_factor[(unit=u, stochastic_scenario=s, t=t)]
+            * ( 
+                + number_of_units[(unit=u, stochastic_scenario=s, t=t)]
                 + expr_sum(
                     units_invested_available[u, s, t1] 
                     for (u, s, t1) in units_invested_available_indices(
@@ -39,7 +40,6 @@ function add_constraint_units_available!(m::Model)
                     init=0
                 )
             )
-            * unit_availability_factor[(unit=u, t=t)] # TODO: Stochastic parameters
         )
         for (u, s, t) in units_on_indices()
     )
