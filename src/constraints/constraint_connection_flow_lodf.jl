33ee97238315b3011914db54d2464448d3b122df
Topi Rasku <topi.rasku@vtt.fi>
2020-07-14 09:20:55 +0300

    #141 Adding `analysis_time` to `connection_from_node` relationship parameters.
    Also style revisions to avoid overlong lines.

--- src/constraints/constraint_connection_flow_lodf.jl
+++ src/constraints/constraint_connection_flow_lodf.jl
@@ -111,9 +111,15 @@ function add_constraint_connection_flow_lodf!(m::Model)
                 )
             ) 
             / minimum(
-                + connection_emergency_capacity[(connection=conn_mon, node=n_mon, direction=d, stochastic_scenario=s, analysis_time=t0, t=t)]
-                * connection_availability_factor[(connection=conn_mon, stochastic_scenario=s, analysis_time=t0, t=t)]
-                * connection_conv_cap_to_flow[(connection=conn_mon, node=n_mon, direction=d, stochastic_scenario=s, analysis_time=t0, t=t)]
+                + connection_emergency_capacity[(
+                    connection=conn_mon, node=n_mon, direction=d, stochastic_scenario=s, analysis_time=t0, t=t
+                )]
+                * connection_availability_factor[(
+                    connection=conn_mon, stochastic_scenario=s, analysis_time=t0, t=t
+                )]
+                * connection_conv_cap_to_flow[(
+                    connection=conn_mon, node=n_mon, direction=d, stochastic_scenario=s, analysis_time=t0, t=t
+                )]
                 for (conn_mon, n_mon, d) in indices(connection_emergency_capacity; connection=conn_mon)
                 for s in s
             )
