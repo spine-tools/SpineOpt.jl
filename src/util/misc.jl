f482102d112ebab423a49b1f63f954c14316c70c
Manuel Marin <manuelma@kth.se>
2020-09-29 21:22:31 -0400

    Fix typo

--- src/util/misc.jl
+++ src/util/misc.jl
@@ -29,7 +29,7 @@ end
 _ObjectArrayLike = Union{ObjectLike,Array{T,1} where T<:ObjectLike}
 _RelationshipArrayLike{K} = NamedTuple{K,V} where {K,V<:Tuple{Vararg{_ObjectArrayLike}}}
 
-function Base.getindex(d::Dict{K,V}, key::_ObjectArrayLike...) where {J,K<:_ArrayRelationshipLike{J},V<:ConstraintRef}
+function Base.getindex(d::Dict{K,V}, key::_ObjectArrayLike...) where {J,K<:_RelationshipArrayLike{J},V<:ConstraintRef}
     Base.getindex(d, NamedTuple{J}(key))
 end
 
