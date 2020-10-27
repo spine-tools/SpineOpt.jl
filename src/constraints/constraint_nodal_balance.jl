1ee956d65a8ef6e43074f99cecfdce993951c178
u0122387 <maren.ihlemann@kuleuven.be>
2020-10-27 23:41:55 +0100

    gc on falsely commited files/changes

--- src/constraints/constraint_nodal_balance.jl
+++ src/constraints/constraint_nodal_balance.jl
@@ -63,7 +63,7 @@ function add_constraint_nodal_balance!(m::Model)
             (n, _internal_nodes(n), s, t)
             for (n, s, t) in node_stochastic_time_indices(m)
             if nodal_balance_sense(node=n) !== :none
-            # && all(balance_type(node=ng) !== :balance_type_group for ng in groups(n))
+            && all(balance_type(node=ng) !== :balance_type_group for ng in groups(n))
         )
     )
 end
