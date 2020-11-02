256cf19e0c19cade9e3b74f19cae282ae79290a7
HJY-VTT <jiangyi.huang@vtt.fi>
2020-11-02 14:36:45 +0200

    add unit-test component w.r.t. units_started_up_coefficient in unit__unit_constraint

--- test/constraints/constraint_unit.jl
+++ test/constraints/constraint_unit.jl
@@ -749,6 +749,7 @@
             unit_flow_coefficient_a = 25
             unit_flow_coefficient_b = 30
             units_on_coefficient = 20
+            units_started_up_coefficient = 35
             objects = [["unit_constraint", "constraint_x"]]
             relationships = [
                 ["unit__from_node__unit_constraint", ["unit_ab", "node_a", "constraint_x"]],
@@ -762,7 +763,8 @@
             relationship_parameter_values = [
                 [relationships[1]..., "unit_flow_coefficient", unit_flow_coefficient_a],
                 [relationships[2]..., "unit_flow_coefficient", unit_flow_coefficient_b],
-                [relationships[3]..., "units_on_coefficient", units_on_coefficient]
+                [relationships[3]..., "units_on_coefficient", units_on_coefficient],
+                [relationships[3]..., "units_started_up_coefficient", units_started_up_coefficient]
             ]
             db_api.import_data_to_url(
                 url_in; 
@@ -774,6 +776,7 @@
             m = run_spineopt(url_in; log_level=0)
             var_unit_flow = m.ext[:variables][:unit_flow]
             var_units_on = m.ext[:variables][:units_on]
+            var_units_started_up = m.ext[:variables][:units_started_up]
             constraint = m.ext[:constraints][:unit_constraint]
             @test length(constraint) == 1
             key_a = (unit(:unit_ab), node(:node_a), direction(:from_node))
@@ -787,7 +790,9 @@
                 * (var_unit_flow[key_a..., s_parent, t1h1] + var_unit_flow[key_a..., s_child, t1h2])
                 + 2 * unit_flow_coefficient_b * var_unit_flow[key_b..., s_parent, t2h]
                 + units_on_coefficient
-                * (var_units_on[unit(:unit_ab), s_parent, t1h1] + var_units_on[unit(:unit_ab), s_child, t1h2]),
+                * (var_units_on[unit(:unit_ab), s_parent, t1h1] + var_units_on[unit(:unit_ab), s_child, t1h2])
+                + units_started_up_coefficient
+                * (var_units_started_up[unit(:unit_ab), s_parent, t1h1] + var_units_started_up[unit(:unit_ab), s_child, t1h2]),
                 Symbol(sense),
                 rhs
             )
@@ -805,6 +810,7 @@
             unit_flow_coefficient_a = 25
             unit_flow_coefficient_b = 30
             units_on_coefficient = 20
+            units_started_up_coefficient = 35
             points = [0.1, 0.5, 1.0]
             operating_points = Dict("type" => "array", "data" => PyVector(points))
             objects = [["unit_constraint", "constraint_x"]]
@@ -822,7 +828,8 @@
                 ["unit__to_node", ["unit_ab", "node_b"], "operating_points", operating_points],
                 [relationships[1]..., "unit_flow_coefficient", unit_flow_coefficient_a],
                 [relationships[2]..., "unit_flow_coefficient", unit_flow_coefficient_b],
-                [relationships[3]..., "units_on_coefficient", units_on_coefficient]
+                [relationships[3]..., "units_on_coefficient", units_on_coefficient],
+                [relationships[3]..., "units_started_up_coefficient", units_started_up_coefficient]
             ]
             db_api.import_data_to_url(
                 url_in; 
@@ -834,6 +841,7 @@
             m = run_spineopt(url_in; log_level=0)
             var_unit_flow_op = m.ext[:variables][:unit_flow_op]
             var_units_on = m.ext[:variables][:units_on]
+            var_units_started_up = m.ext[:variables][:units_started_up]
             constraint = m.ext[:constraints][:unit_constraint]
             @test length(constraint) == 1
             key_a = (unit(:unit_ab), node(:node_a), direction(:from_node))
@@ -850,7 +858,9 @@
                 )
                 + 2 * sum(unit_flow_coefficient_b * var_unit_flow_op[key_b..., i, s_parent, t2h] for i in 1:3)
                 + units_on_coefficient
-                * (var_units_on[unit(:unit_ab), s_parent, t1h1] + var_units_on[unit(:unit_ab), s_child, t1h2]),
+                * (var_units_on[unit(:unit_ab), s_parent, t1h1] + var_units_on[unit(:unit_ab), s_child, t1h2])
+                + units_started_up_coefficient
+                * (var_units_started_up[unit(:unit_ab), s_parent, t1h1] + var_units_started_up[unit(:unit_ab), s_child, t1h2]),
                 Symbol(sense),
                 rhs
             )
