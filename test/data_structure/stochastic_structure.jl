8b531be7f5e174d7394dd35ae56a22c2651199a6
Topi Rasku <topi.rasku@vtt.fi>
2020-11-17 14:03:32 +0200

    #215 Style revision.

--- test/data_structure/stochastic_structure.jl
+++ test/data_structure/stochastic_structure.jl
@@ -17,150 +17,144 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #############################################################################
 
-function _model()
-    m = Model()
-    m.ext[:instance] = first(model())
-    m
-end
-
-url_in = "sqlite:///$(@__DIR__)/test.sqlite"
-test_data = Dict(
-    :objects => [
-        ["model", "instance"], 
-        ["node", "only_node"],
-        ["unit", "only_unit"],
-        ["temporal_block", "only_block"],
-        ["stochastic_structure", "structure_a"],
-        ["stochastic_structure", "structure_b"],
-        ["stochastic_scenario", "scenario_a"],
-        ["stochastic_scenario", "scenario_a1"],
-        ["stochastic_scenario", "scenario_a2"],
-        ["stochastic_scenario", "scenario_b"],
-        ["stochastic_scenario", "scenario_b1"],
-        ["stochastic_scenario", "scenario_b2"]
-    ],
-    :relationships => [
-        ["model__temporal_block", ["instance", "only_block"]],
-        ["model__stochastic_structure", ["instance", "structure_a"]],
-        ["model__stochastic_structure", ["instance", "structure_b"]],
-        ["node__temporal_block", ["only_node", "only_block"]],
-        ["node__stochastic_structure", ["only_node", "structure_a"]],
-        ["units_on__temporal_block", ["only_unit", "only_block"]],
-        ["units_on__stochastic_structure", ["only_unit", "structure_b"]],
-        ["parent_stochastic_scenario__child_stochastic_scenario", ["scenario_a", "scenario_a1"]],
-        ["parent_stochastic_scenario__child_stochastic_scenario", ["scenario_a", "scenario_a2"]],
-        ["parent_stochastic_scenario__child_stochastic_scenario", ["scenario_b", "scenario_b1"]],
-        ["parent_stochastic_scenario__child_stochastic_scenario", ["scenario_b", "scenario_b2"]],
-        ["stochastic_structure__stochastic_scenario", ["structure_a", "scenario_a"]],
-        ["stochastic_structure__stochastic_scenario", ["structure_a", "scenario_a1"]],
-        ["stochastic_structure__stochastic_scenario", ["structure_a", "scenario_a2"]],
-        ["stochastic_structure__stochastic_scenario", ["structure_a", "scenario_b"]],
-        ["stochastic_structure__stochastic_scenario", ["structure_a", "scenario_b1"]],
-        ["stochastic_structure__stochastic_scenario", ["structure_a", "scenario_b2"]],
-        ["stochastic_structure__stochastic_scenario", ["structure_b", "scenario_b"]],
-        ["stochastic_structure__stochastic_scenario", ["structure_b", "scenario_b1"]],
-        ["stochastic_structure__stochastic_scenario", ["structure_b", "scenario_b2"]],
-        ["stochastic_structure__stochastic_scenario", ["structure_b", "scenario_a"]],
-        ["stochastic_structure__stochastic_scenario", ["structure_b", "scenario_a1"]],
-        ["stochastic_structure__stochastic_scenario", ["structure_b", "scenario_a2"]],
-    ],
-    :object_parameter_values => [
-        ["model", "instance", "model_start", Dict("type" => "date_time", "data" => "2000-01-01T00:00:00")],
-        ["model", "instance", "model_end", Dict("type" => "date_time", "data" => "2000-01-04T00:00:00")],
-        ["temporal_block", "only_block", "resolution", Dict("type" => "duration", "data" => "1D")],
-    ],
-    :relationship_parameter_values => [
-        [
-            "stochastic_structure__stochastic_scenario",
-            ["structure_a", "scenario_a"],
-            "stochastic_scenario_end",
-            Dict("type" => "duration", "data" => "1D")
-        ],
-        [
-            "stochastic_structure__stochastic_scenario",
-            ["structure_a", "scenario_a"],
-            "weight_relative_to_parents",
-            1.0
-        ],
-        [
-            "stochastic_structure__stochastic_scenario",
-            ["structure_a", "scenario_a1"],
-            "weight_relative_to_parents",
-            1.0
-        ],
-        [
-            "stochastic_structure__stochastic_scenario",
-            ["structure_a", "scenario_a2"],
-            "weight_relative_to_parents",
-            2.0
-        ],
-        [
-            "stochastic_structure__stochastic_scenario",
-            ["structure_a", "scenario_b"],
-            "weight_relative_to_parents",
-            0.0
-        ],
-        [
-            "stochastic_structure__stochastic_scenario",
-            ["structure_a", "scenario_b1"],
-            "weight_relative_to_parents",
-            0.0
-        ],
-        [
-            "stochastic_structure__stochastic_scenario",
-            ["structure_a", "scenario_b2"],
-            "weight_relative_to_parents",
-            0.0
-        ],
-        [
-            "stochastic_structure__stochastic_scenario",
-            ["structure_b", "scenario_b"],
-            "stochastic_scenario_end",
-            Dict("type" => "duration", "data" => "2D")
-        ],
-        [
-            "stochastic_structure__stochastic_scenario",
-            ["structure_b", "scenario_b"],
-            "weight_relative_to_parents",
-            2.0
-        ],
-        [
-            "stochastic_structure__stochastic_scenario",
-            ["structure_b", "scenario_b1"],
-            "weight_relative_to_parents",
-            0.1
-        ],
-        [
-            "stochastic_structure__stochastic_scenario",
-            ["structure_b", "scenario_b2"],
-            "weight_relative_to_parents",
-            0.2
-        ],
-        [
-            "stochastic_structure__stochastic_scenario",
-            ["structure_b", "scenario_a"],
-            "weight_relative_to_parents",
-            0.0
-        ],
-        [
-            "stochastic_structure__stochastic_scenario",
-            ["structure_b", "scenario_a1"],
-            "weight_relative_to_parents",
-            0.0
-        ],
-        [
-            "stochastic_structure__stochastic_scenario",
-            ["structure_b", "scenario_a2"],
-            "weight_relative_to_parents",
-            0.0
-        ],
-    ]
-)
-_load_template(url_in)
-db_api.import_data_to_url(url_in; test_data...)
-m = run_spineopt(url_in, log_level=0, optimize=false)
-
 @testset "stochastic structure" begin
+    url_in = "sqlite:///$(@__DIR__)/test.sqlite"
+    test_data = Dict(
+        :objects => [
+            ["model", "instance"], 
+            ["node", "only_node"],
+            ["unit", "only_unit"],
+            ["temporal_block", "only_block"],
+            ["stochastic_structure", "structure_a"],
+            ["stochastic_structure", "structure_b"],
+            ["stochastic_scenario", "scenario_a"],
+            ["stochastic_scenario", "scenario_a1"],
+            ["stochastic_scenario", "scenario_a2"],
+            ["stochastic_scenario", "scenario_b"],
+            ["stochastic_scenario", "scenario_b1"],
+            ["stochastic_scenario", "scenario_b2"]
+        ],
+        :relationships => [
+            ["model__temporal_block", ["instance", "only_block"]],
+            ["model__stochastic_structure", ["instance", "structure_a"]],
+            ["model__stochastic_structure", ["instance", "structure_b"]],
+            ["node__temporal_block", ["only_node", "only_block"]],
+            ["node__stochastic_structure", ["only_node", "structure_a"]],
+            ["units_on__temporal_block", ["only_unit", "only_block"]],
+            ["units_on__stochastic_structure", ["only_unit", "structure_b"]],
+            ["parent_stochastic_scenario__child_stochastic_scenario", ["scenario_a", "scenario_a1"]],
+            ["parent_stochastic_scenario__child_stochastic_scenario", ["scenario_a", "scenario_a2"]],
+            ["parent_stochastic_scenario__child_stochastic_scenario", ["scenario_b", "scenario_b1"]],
+            ["parent_stochastic_scenario__child_stochastic_scenario", ["scenario_b", "scenario_b2"]],
+            ["stochastic_structure__stochastic_scenario", ["structure_a", "scenario_a"]],
+            ["stochastic_structure__stochastic_scenario", ["structure_a", "scenario_a1"]],
+            ["stochastic_structure__stochastic_scenario", ["structure_a", "scenario_a2"]],
+            ["stochastic_structure__stochastic_scenario", ["structure_a", "scenario_b"]],
+            ["stochastic_structure__stochastic_scenario", ["structure_a", "scenario_b1"]],
+            ["stochastic_structure__stochastic_scenario", ["structure_a", "scenario_b2"]],
+            ["stochastic_structure__stochastic_scenario", ["structure_b", "scenario_b"]],
+            ["stochastic_structure__stochastic_scenario", ["structure_b", "scenario_b1"]],
+            ["stochastic_structure__stochastic_scenario", ["structure_b", "scenario_b2"]],
+            ["stochastic_structure__stochastic_scenario", ["structure_b", "scenario_a"]],
+            ["stochastic_structure__stochastic_scenario", ["structure_b", "scenario_a1"]],
+            ["stochastic_structure__stochastic_scenario", ["structure_b", "scenario_a2"]],
+        ],
+        :object_parameter_values => [
+            ["model", "instance", "model_start", Dict("type" => "date_time", "data" => "2000-01-01T00:00:00")],
+            ["model", "instance", "model_end", Dict("type" => "date_time", "data" => "2000-01-04T00:00:00")],
+            ["temporal_block", "only_block", "resolution", Dict("type" => "duration", "data" => "1D")],
+        ],
+        :relationship_parameter_values => [
+            [
+                "stochastic_structure__stochastic_scenario",
+                ["structure_a", "scenario_a"],
+                "stochastic_scenario_end",
+                Dict("type" => "duration", "data" => "1D")
+            ],
+            [
+                "stochastic_structure__stochastic_scenario",
+                ["structure_a", "scenario_a"],
+                "weight_relative_to_parents",
+                1.0
+            ],
+            [
+                "stochastic_structure__stochastic_scenario",
+                ["structure_a", "scenario_a1"],
+                "weight_relative_to_parents",
+                1.0
+            ],
+            [
+                "stochastic_structure__stochastic_scenario",
+                ["structure_a", "scenario_a2"],
+                "weight_relative_to_parents",
+                2.0
+            ],
+            [
+                "stochastic_structure__stochastic_scenario",
+                ["structure_a", "scenario_b"],
+                "weight_relative_to_parents",
+                0.0
+            ],
+            [
+                "stochastic_structure__stochastic_scenario",
+                ["structure_a", "scenario_b1"],
+                "weight_relative_to_parents",
+                0.0
+            ],
+            [
+                "stochastic_structure__stochastic_scenario",
+                ["structure_a", "scenario_b2"],
+                "weight_relative_to_parents",
+                0.0
+            ],
+            [
+                "stochastic_structure__stochastic_scenario",
+                ["structure_b", "scenario_b"],
+                "stochastic_scenario_end",
+                Dict("type" => "duration", "data" => "2D")
+            ],
+            [
+                "stochastic_structure__stochastic_scenario",
+                ["structure_b", "scenario_b"],
+                "weight_relative_to_parents",
+                2.0
+            ],
+            [
+                "stochastic_structure__stochastic_scenario",
+                ["structure_b", "scenario_b1"],
+                "weight_relative_to_parents",
+                0.1
+            ],
+            [
+                "stochastic_structure__stochastic_scenario",
+                ["structure_b", "scenario_b2"],
+                "weight_relative_to_parents",
+                0.2
+            ],
+            [
+                "stochastic_structure__stochastic_scenario",
+                ["structure_b", "scenario_a"],
+                "weight_relative_to_parents",
+                0.0
+            ],
+            [
+                "stochastic_structure__stochastic_scenario",
+                ["structure_b", "scenario_a1"],
+                "weight_relative_to_parents",
+                0.0
+            ],
+            [
+                "stochastic_structure__stochastic_scenario",
+                ["structure_b", "scenario_a2"],
+                "weight_relative_to_parents",
+                0.0
+            ],
+        ]
+    )
+    _load_template(url_in)
+    db_api.import_data_to_url(url_in; test_data...)
+    m = run_spineopt(url_in, log_level=0, optimize=false)
+
     @testset "node_stochastic_time_indices" begin
         @test length(node_stochastic_time_indices(m; stochastic_scenario=stochastic_scenario(:scenario_a))) == 1
         @test length(node_stochastic_time_indices(m; stochastic_scenario=stochastic_scenario(:scenario_a1))) == 2
