dfddb9f5fd579997361ec7499f35cc2706e500a4
T5600\Jody <jody.dillon@smartwires.com>
2020-12-10 12:21:17 +0100

    WIP: Expand scope of unit_constraint.  - Currently working

--- src/constraints/constraint_unit_constraint.jl
+++ src/constraints/constraint_unit_constraint.jl
@@ -22,9 +22,17 @@
 
 Custom constraint for `units`.
 """
-function add_constraint_unit_constraint!(m::Model)
-    @fetch unit_flow_op, unit_flow, units_on, units_started_up = m.ext[:variables]
+function add_constraint_unit_constraint!(m::Model)       
+    @fetch unit_flow_op, unit_flow, units_on, units_started_up, connection_flow, node_state = m.ext[:variables]
     t0 = startref(current_window(m))
+    #=
+    for (uc, s, t) in constraint_unit_constraint_indices(m)
+        @info "indices" uc s t
+        for n in node__unit_constraint(unit_constraint=uc)
+            @info "demand" n demand(node=n, stochastic_scenario=s, analysis_time=t0, t=t)
+        end
+    end
+    =#
     m.ext[:constraints][:unit_constraint] = Dict(
         (unit_constraint=uc, stochastic_path=s, t=t) => sense_constraint(
             m,
@@ -150,7 +158,7 @@ function add_constraint_unit_constraint!(m::Model)
                     analysis_time=t0,
                     t=t_short,
                 )] *
-                duration(t_short) for (c, n) in indices(connection_form_flow_uc_coefficient; unit_constraint=uc)
+                duration(t_short) for (c, n, uc) in indices(connection_from_flow_uc_coefficient; unit_constraint=uc)
                 for
                 (c, n, d, s, t_short) in connection_flow_indices(
                     m;
@@ -172,7 +180,7 @@ function add_constraint_unit_constraint!(m::Model)
                     analysis_time=t0,
                     t=t_short,
                 )] *
-                duration(t_short) for (c, n) in indices(connection_to_flow_uc_coefficient; unit_constraint=uc)
+                duration(t_short) for (c, n, uc) in indices(connection_to_flow_uc_coefficient; unit_constraint=uc)
                 for
                 (c, n, d, s, t_short) in connection_flow_indices(
                     m;
@@ -204,23 +212,11 @@ function add_constraint_unit_constraint!(m::Model)
                 init=0,
             )
             +expr_sum(
-                +node_demand[(
-                    node=n,
-                    stochastic_scenario=s,
-                    analysis_time=t0,
-                    t=t_short,
-                )] * duration(t_short) 
-                *expr_sum(
-                    +node_demand_uc_coefficient[(
-                        node=n,
-                        stochastic_scenario=s,
-                        analysis_time=t0,
-                        t=t_short,
-                    )] * duration(t_short),
-                    for  (n, s, t_short) in indices(node_demand_uc_coefficient; unit_constraint=uc);
-                    init=0,
-                ),
-                for  (n, s, t_short) in indices(node_demand);
+                +demand[(node=n, stochastic_scenario=s, analysis_time=t0, t=t)]                 
+                    *demand_uc_coefficient[(node=n, unit_constraint=uc, stochastic_scenario=s, analysis_time=t0, t=t)]
+                    *duration(t)
+                for n in node__unit_constraint(unit_constraint=uc)
+                for (ns, s, t_short) in node_stochastic_time_indices(m; node=n, stochastic_scenario=s, t=t_in_t(m; t_long=t))
                 init=0,            
             ),
             constraint_sense(unit_constraint=uc),
@@ -263,17 +259,17 @@ Find the lowest temporal resolution amoung the `unit_flow` variables appearing i
 """
 function _constraint_unit_constraint_lowest_resolution_t(m, uc, t)
     t_lowest_resolution(
-        collect(
-            ind.t for unit__node__unit_constraint in (unit__from_node__unit_constraint, unit__to_node__unit_constraint)
+        vcat(
+            [ind.t for unit__node__unit_constraint in (unit__from_node__unit_constraint, unit__to_node__unit_constraint)
             for (u, n) in unit__node__unit_constraint(unit_constraint=uc)
-            for ind in unit_flow_indices(m; unit=u, node=n, t=t),
-            
-            ind.t for connection__node__unit_constraint in (connection__from_node__unit_constraint, connection__to_node__unit_constraint)
+            for ind in unit_flow_indices(m; unit=u, node=n, t=t)],            
+            [ind.t for connection__node__unit_constraint in (connection__from_node__unit_constraint, connection__to_node__unit_constraint)
             for (c, n) in connection__node__unit_constraint(unit_constraint=uc)
-            for ind in connection_flow_indices(m; connection=c, node=n, t=t),
-
-            ind.t for n in node__unit_constraint(unit_constraint=uc)
-            for ind in node_state_indices(m; node=n, t=t)
+            for ind in connection_flow_indices(m; connection=c, node=n, t=t)],
+            [ind.t for n in node__unit_constraint(unit_constraint=uc)
+            for ind in node_state_indices(m; node=n, t=t)],
+            [ind.t for n in node__unit_constraint(unit_constraint=uc)
+            for ind in node_stochastic_time_indices(m; node=n, t=t)],
         )
     )
 end
@@ -301,6 +297,19 @@ function _constraint_unit_constraint_connection_flow_indices(m, uc, t)
     (
         ind for (c, n) in unit__from_node__unit_constraint(unit_constraint=uc) for
         ind in connection_flow_indices(m; connection=c, node=n, direction=direction(:from_node), t=t_in_t(m; t_long=t))
+    )
+end
+
+
+"""
+    _constraint_unit_constraint_node_stochastic_time_indices(m, uc, t)
+
+Gather the `node_stochastic_time_indices` indices appearing in `add_constraint_unit_constraint!`.
+"""
+function _constraint_unit_constraint_node_stochastic_time_indices(m, uc, t)
+    (
+        ind for n in node__unit_constraint(unit_constraint=uc) for
+        ind in node_stochastic_time_indices(m; node=n, t=t_in_t(m; t_long=t))
     )
 end
 
@@ -341,5 +350,6 @@ function _constraint_unit_constraint_indices(m, uc, t)
         _constraint_unit_constraint_units_on_indices(m, uc, t),
         _constraint_unit_constraint_connection_flow_indices(m, uc, t),
         _constraint_unit_constraint_node_state_indices(m, uc, t),
+        _constraint_unit_constraint_node_stochastic_time_indices(m, uc, t),
     ))
 end
