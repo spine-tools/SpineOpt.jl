spine-tools/SpineOpt.jl

commit 1f55ce064c7c9750a2a942a162a347075970ce75

Start using entity groups instead of relationships for grouping
Re #178

src/constraints/constraint_node_injection.jl
@@ -67,40 +67,38 @@ Set the node injection equal to the summation of all 'input' flows but connectio
 """
 function add_constraint_node_injection!(m::Model)
     @fetch node_injection, node_state, unit_flow = m.ext[:variables]
-    # TODO: We need to include both: storages that are defined on ng and storage that are defined on internal nodes
+    # TODO: We need to include both: storages that are defined on n and storage that are defined on internal nodes
     m.ext[:constraints][:node_injection] = Dict(
-        (ng, stochastic_path, t_before, t_after) => @constraint(
+        (n, stochastic_path, t_before, t_after) => @constraint(
             m,
             + expr_sum(
-                + node_injection[ng, s, t_after]
-                for (ng, s, t_after) in node_injection_indices(
-                    node=ng, stochastic_scenario=stochastic_path, t=t_after
-                );
+                + node_injection[n, s, t_after]
+                for (n, s, t) in node_injection_indices(node=n, stochastic_scenario=stochastic_path, t=t_after);
                 init=0
             )
             ==
             + expr_sum(
                 (
-                    + get(node_state, (ng, s, t_before), 0) * state_coeff[(node=ng, t=t_before)]
-                    - get(node_state, (ng, s, t_after), 0) * state_coeff[(node=ng, t=t_after)]
+                    + get(node_state, (n, s, t_before), 0) * state_coeff[(node=n, t=t_before)]
+                    - get(node_state, (n, s, t_after), 0) * state_coeff[(node=n, t=t_after)]
                 )
                 / duration(t_after)
                 # Self-discharge commodity losses
-                - get(node_state, (ng, s, t_after), 0) * frac_state_loss[(node=ng, t=t_after)]
+                - get(node_state, (n, s, t_after), 0) * frac_state_loss[(node=n, t=t_after)]
                 for s in stochastic_path;
                 init=0
             )
             # Diffusion of commodity from other nodes to this one
             + expr_sum(
-                get(node_state, (other_node, s, t_after), 0) * diff_coeff[(node1=other_node, node2=ng, t=t_after)]
-                for other_node in node__node(node2=ng)
+                get(node_state, (other_node, s, t_after), 0) * diff_coeff[(node1=other_node, node2=n, t=t_after)]
+                for other_node in node__node(node2=n)
                 for s in stochastic_path;
                 init=0
             )
             # Diffusion of commodity from this node to other nodes
             - expr_sum(
-                get(node_state, (ng, s, t_after), 0) * diff_coeff[(node1=ng, node2=other_node, t=t_after)]
-                for other_node in node__node(node1=ng)
+                get(node_state, (n, s, t_after), 0) * diff_coeff[(node1=n, node2=other_node, t=t_after)]
+                for other_node in node__node(node1=n)
                 for s in stochastic_path;
                 init=0
             )
@@ -108,7 +106,10 @@ function add_constraint_node_injection!(m::Model)
             + expr_sum(
                 unit_flow[u, n, d, s, t_short]
                 for (u, n, d, s, t_short) in unit_flow_indices(
-                    node=ng, direction=direction(:to_node), stochastic_scenario=stochastic_path, t=t_in_t(t_long=t_after)
+                    node=n, 
+                    direction=direction(:to_node), 
+                    stochastic_scenario=stochastic_path, 
+                    t=t_in_t(t_long=t_after)
                 );
                 init=0
             )
@@ -116,18 +117,20 @@ function add_constraint_node_injection!(m::Model)
             - expr_sum(
                 unit_flow[u, n, d, s, t_short]
                 for (u, n, d, s, t_short) in unit_flow_indices(
-                    node=ng, direction=direction(:from_node), stochastic_scenario=stochastic_path, t=t_in_t(t_long=t_after)
+                    node=n, 
+                    direction=direction(:from_node), 
+                    stochastic_scenario=stochastic_path, 
+                    t=t_in_t(t_long=t_after)
                 );
                 init=0
             )
-            - demand[(node=ng, t=t_after)]
+            - demand[(node=n, t=t_after)]
             - expr_sum(
-                fractional_demand[(node1=ng_, node2=ng, t=t_after)] * demand[(node=ng_, t=t_after)]
-                for ng_ in node_group__node(node2=ng);
+                fractional_demand[(node=n, t=t_after)] * demand[(node=ng, t=t_after)] for ng in groups(n);
                 init=0
             )
             # TODO: fractional_demand etc. are scenario dependent?
         )
-        for (ng, stochastic_path, t_before, t_after) in constraint_node_injection_indices()
+        for (n, stochastic_path, t_before, t_after) in constraint_node_injection_indices()
     )
 end

Project.toml
@@ -21,4 +21,4 @@ TimeZones = "f269a46b-ccf7-5d73-abea-4c690281aa53"
 [compat]
 julia = "^1.2"
 JuMP = "0.21.2"
-SpineInterface = "0.2.3"
+SpineInterface = "0.3.0"

src/constraints/constraint_connection_flow_capacity.jl
@@ -28,9 +28,7 @@ function constraint_connection_flow_capacity_indices()
     unique(
         (connection=c, node=ng, direction=d, stochastic_path=path, t=t)
         for (c, ng, d) in indices(connection_capacity)
-        # TODO: do we need to expand groups here? We still get the 'groups' out of `indices(connection_capacity)`,
-        # and then we feed them to `connection_flow_indices` in the constraint below (at which point they get expanded).
-        for t in time_slice(temporal_block=node__temporal_block(node=expand_node_group(ng))) #expand node_group?
+        for t in time_slice(temporal_block=node__temporal_block(node=members(ng)))
         for path in active_stochastic_paths(
             unique(ind.stochastic_scenario for ind in connection_flow_indices(connection=c, node=ng, direction=d, t=t))
         )

src/constraints/constraint_max_nonspin_ramp_up.jl
@@ -29,7 +29,7 @@ function constraint_max_nonspin_ramp_up_indices()
     unique(
         (unit=u, node=ng, direction=d, stochastic_path=path, t=t)
         for (u, ng, d) in indices(max_res_startup_ramp)
-        for t in time_slice(temporal_block=node__temporal_block(node=expand_node_group(ng)))
+        for t in time_slice(temporal_block=node__temporal_block(node=members(ng)))
         for path in active_stochastic_paths(
             unique(
                 ind.stochastic_scenario for ind in Iterators.flatten(

src/constraints/constraint_max_start_up_ramp.jl
@@ -28,7 +28,7 @@ function constraint_max_start_up_ramp_indices()
     unique(
         (unit=u, node=ng, direction=d, stochastic_path=path, t=t)
         for (u, ng, d) in indices(max_startup_ramp)
-        for t in t_lowest_resolution(time_slice(temporal_block=node__temporal_block(node=expand_node_group(ng))))
+        for t in t_lowest_resolution(time_slice(temporal_block=node__temporal_block(node=members(ng))))
         # How to deal with groups correctly?
         for path in active_stochastic_paths(
             unique(

src/constraints/constraint_min_nonspin_ramp_up.jl
@@ -28,7 +28,7 @@ function constraint_min_nonspin_ramp_up_indices()
     unique(
         (unit=u, node=ng, direction=d, stochastic_path=path, t=t)
         for (u, ng, d) in indices(min_res_startup_ramp)
-        for t in time_slice(temporal_block=node__temporal_block(node=expand_node_group(ng)))
+        for t in time_slice(temporal_block=node__temporal_block(node=members(ng)))
         for path in active_stochastic_paths(
             unique(
                 ind.stochastic_scenario for ind in Iterators.flatten(

src/constraints/constraint_min_start_up_ramp.jl
@@ -28,7 +28,7 @@ function constraint_min_start_up_ramp_indices()
     unique(
         (unit=u, node=ng, direction=d, stochastic_path=path, t=t)
         for (u, ng, d) in indices(min_startup_ramp)
-        for t in t_lowest_resolution(time_slice(temporal_block=node__temporal_block(node=expand_node_group(ng))))
+        for t in t_lowest_resolution(time_slice(temporal_block=node__temporal_block(node=members(ng))))
         # How to deal with groups correctly?
         for path in active_stochastic_paths(
             unique(

src/constraints/constraint_nodal_balance.jl
@@ -16,8 +16,8 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #############################################################################
-#TODO: support nodal balance sense
-#TODO: as proposed in the wiki on groups: We should be able to support
+# TODO: support nodal balance sense
+# TODO: as proposed in the wiki on groups: We should be able to support
 # a) node_balance for node group and NO balance for underlying node
 # b) node_balance for node group AND balance for underlying node
 
@@ -63,12 +63,12 @@ function add_constraint_nodal_balance!(m::Model)
             (n, _internal_nodes(n), s, t)
             for (n, s, t) in node_stochastic_time_indices()
             if nodal_balance_sense(node=n) !== :none
-            && all(balance_type(node=ng) !== :balance_type_group for ng in node_group__node(node2=n))
+            && all(balance_type(node=ng) !== :balance_type_group for ng in groups(n))
         )
     )
 end
 
-_internal_nodes(n::Object) = balance_type(node=n) === :balance_type_group ? node_group__node(node1=n) : []
+_internal_nodes(n::Object) = balance_type(node=n) === :balance_type_group ? members(n) : []
 
 """
     _connection_nodes(conn)

commit ee63b9583992cebfd0d578f7fe4a3ca7d26fc13f

Fix first term of constraint node injection
This bug was introduced back in 1f55ce064c7c9750a2a942a162a347075970ce75
by a careless index renaming.

src/constraints/constraint_node_injection.jl
@@ -30,7 +30,7 @@ function add_constraint_node_injection!(m::Model)
         (node=n, stochastic_path=s, t_before=t_before, t_after=t_after) => @constraint(
             m,
             + expr_sum(
-                + node_injection[n, s, t_after] + demand[
+                + node_injection[n, s, t] + demand[
                     (
                         node=n,
                         stochastic_scenario=s,