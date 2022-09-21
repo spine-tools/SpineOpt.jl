spine-tools/SpineOpt.jl

commit 1a14541933fa0d58189ffb3c5a662dac97e15835

Fixing the issue with division by zero in `constraint_unit_flow_capacity`

src/constraints/constraint_unit_flow_capacity.jl
@@ -32,8 +32,6 @@ function add_constraint_unit_flow_capacity!(m::Model)
             m,
             expr_sum(
                 + unit_flow[u, n, d, s, t] * duration(t)
-                / unit_capacity[(unit=u, node=ng, direction=d, stochastic_scenario=s, analysis_time=t0, t=t)]
-                / unit_conv_cap_to_flow[(unit=u, node=ng, direction=d, stochastic_scenario=s, analysis_time=t0, t=t)]
                 for (u, n, d, s, t) in unit_flow_indices(
                     m;
                     unit=u,
@@ -50,6 +48,18 @@ function add_constraint_unit_flow_capacity!(m::Model)
                 * min(duration(t1), duration(t))
                 for (u, s, t1) in units_on_indices(m; unit=u, stochastic_scenario=s, t=t_overlaps_t(m; t=t));
                 init=0,
+            )
+            * sum(
+                unit_capacity[(unit=u, node=ng, direction=d, stochastic_scenario=s, analysis_time=t0, t=t)]
+                * unit_conv_cap_to_flow[(unit=u, node=ng, direction=d, stochastic_scenario=s, analysis_time=t0, t=t)]
+                for (u, n, d, s, t) in unit_flow_indices(
+                    m;
+                    unit=u,
+                    node=ng,
+                    direction=d,
+                    stochastic_scenario=s,
+                    t=t_in_t(m; t_long=t),
+                )
             )
         ) for (u, ng, d, s, t) in constraint_unit_flow_capacity_indices(m)
     )

commit dad8a05ff7e9919b65a6d2b91c8fb13bfd471518

Bug fix: restore group capacity functionality
This bug was introduced in commit 1a14541933fa0d58189ffb3c5a662dac97e15835 after a series of other adaption. Mainly we summed up the unit_capacity for each unit_flow, while it should be the sum of unit_flows that is constrained by one shared unit_capacity parameter.
Bottom line we now retrieve the stochastic scenario for the unit_capacity parameter from the units_on variable again, to restore functionlaity. #483 discusses the problem for other cases

src/constraints/constraint_unit_flow_capacity.jl
@@ -47,20 +47,10 @@ function add_constraint_unit_flow_capacity!(m::Model)
             + expr_sum(
                 (units_on[u, s, t1])
                 * min(duration(t1), duration(t))
+                * unit_capacity[(unit=u, node=ng, direction=d, stochastic_scenario=s, analysis_time=t0, t=t)]
+                * unit_conv_cap_to_flow[(unit=u, node=ng, direction=d, stochastic_scenario=s, analysis_time=t0, t=t)]
                 for (u, s, t1) in units_on_indices(m; unit=u, stochastic_scenario=s, t=t_overlaps_t(m; t=t));
                 init=0,
-            )
-            * sum(
-                unit_capacity[(unit=u, node=ng, direction=d, stochastic_scenario=s, analysis_time=t0, t=t)]
-                * unit_conv_cap_to_flow[(unit=u, node=ng, direction=d, stochastic_scenario=s, analysis_time=t0, t=t)]
-                for (u, n, d, s, t) in unit_flow_indices(
-                    m;
-                    unit=u,
-                    node=ng,
-                    direction=d,
-                    stochastic_scenario=s,
-                    t=t_in_t(m; t_long=t),
-                )
             )
         ) for (u, ng, d, s, t) in constraint_unit_flow_capacity_indices(m)
     )