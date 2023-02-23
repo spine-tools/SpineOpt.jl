12602833bf5c22e3b0b183e489be737cb16d02cd
Manuel <chacarero@gmail.com>
2023-02-23 17:50:23 +0100

    Improve message

--- src/run_spineopt_standard.jl
+++ src/run_spineopt_standard.jl
@@ -36,7 +36,11 @@ function rerun_spineopt!(
     @timelog log_level 2 "Creating temporal structure..." generate_temporal_structure!(m)
     @timelog log_level 2 "Creating stochastic structure..." generate_stochastic_structure!(m)
     roll_count = _roll_count(m)
-    @timelog log_level 2 "Bringing model to the last window..." roll_temporal_structure!(m, roll_count)
+    @log log_level 2 """
+    NOTE: We will first build the model for the last optimisation window to make sure it can roll that far.
+    Then we will bring the model to the first window to start solving it.
+    """
+    roll_temporal_structure!(m, roll_count)
     init_model!(
         m;
         add_user_variables=add_user_variables,
