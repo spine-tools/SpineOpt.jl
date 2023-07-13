6e506f5d3fc88d6e9c2b65c70085a0a8b6f8dd00
Jody Dillon <jodyddillon@gmail.com>
2023-07-13 08:31:18 +0200

    Removed using CLP, CBC from RunSpineOpt

--- src/run_spineopt.jl
+++ src/run_spineopt.jl
@@ -17,9 +17,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #############################################################################
 
-using Cbc
-using Clp
-
 module _Template
 using SpineInterface
 end
