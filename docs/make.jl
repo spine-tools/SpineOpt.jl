737ba95a217dcf1980b4a948164b25b61b930520
Manuel <chacarero@gmail.com>
2023-09-28 09:03:22 +0200

    Fix docs to work with latest Documenter.jl

--- docs/make.jl
+++ docs/make.jl
@@ -75,5 +75,6 @@ makedocs(
     sitename="SpineOpt.jl",
     format=Documenter.HTML(prettyurls=get(ENV, "CI", nothing) == "true"),  # uncomment to deploy locally
     pages=pages,
+    warnonly=true
 )
 deploydocs(repo="github.com/spine-tools/SpineOpt.jl.git", versions=["stable" => "v^", "v#.#"])
