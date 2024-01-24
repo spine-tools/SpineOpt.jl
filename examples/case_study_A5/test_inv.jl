a05dd76a3aa29409329e1f6b3ba2b4bed7766ab7
datejada <diego.tejadaarango@tno.nl>
2024-01-24 16:18:52 +0100

    Move test_inv file

--- examples/case_study_A5/test_inv.jl
+++
@@ -1,7 +0,0 @@
-###test_inv
-using Revise
-using SpineOpt
-
-db_url_in = "sqlite:///Data Store 1 - Copy.sqlite"
-db_url_out = "sqlite:///Data Store 1 - Copy_out.sqlite"
-m = run_spineopt(db_url_in, db_url_out)
