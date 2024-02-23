03506460aa374f99a180cc275d0fd6f542ef807e
Lauren Clisby <lclisby@gmail.com>
2024-02-23 17:02:49 +0100

    Add logo to docs (#889)
    * Add logo to docs

--- docs/make.jl
+++ docs/make.jl
@@ -16,7 +16,7 @@ default_translation = Dict(
     ["object_classes"] => "Object Classes",
 )
 concept_dictionary = add_cross_references!(
-    initialize_concept_dictionary(SpineOpt.template(); translation=default_translation),
+    initialize_concept_dictionary(SpineOpt.template(); translation = default_translation),
 )
 write_concept_reference_files(concept_dictionary, path)
 
@@ -41,7 +41,7 @@ pages = [
         "Setting up a workflow" => joinpath("getting_started", "setup_workflow.md"),
         "Creating Your Own Model" => joinpath("getting_started", "creating_your_own_model.md"),
         "Archetypes" => joinpath("getting_started", "archetypes.md"),
-        "Managing Outputs" => joinpath("getting_started", "output_data.md")
+        "Managing Outputs" => joinpath("getting_started", "output_data.md"),
     ],
     "Tutorials" => Any[
         "Webinars" => joinpath("tutorial", "webinars.md"),
@@ -50,7 +50,7 @@ pages = [
         "Ramping constraints" => joinpath("tutorial", "ramping.md"),
         "Unit Commitment" => joinpath("tutorial", "unit_commitment.md"),
         "Two hydro plants" => joinpath("tutorial", "tutorialTwoHydro.md"),
-        "Case Study A5" => joinpath("tutorial", "case_study_a5.md")
+        "Case Study A5" => joinpath("tutorial", "case_study_a5.md"),
     ],
     "How to" => [],
     "Concept Reference" => Any[
@@ -78,7 +78,7 @@ pages = [
         "Pressure driven gas transfer" => joinpath("advanced_concepts", "pressure_driven_gas_transfer.md"),
         "Lossless nodal DC power flows" => joinpath("advanced_concepts", "Lossless_DC_power_flow.md"),
         "Representative days with seasonal storages" => joinpath(
-            "advanced_concepts", "representative_days_w_seasonal_storage.md"
+            "advanced_concepts", "representative_days_w_seasonal_storage.md",
         ),
         "Imposing renewable energy targets" => joinpath("advanced_concepts", "cumulated_flow_restrictions.md"),
         "Modelling to generate alternatives" => joinpath("advanced_concepts", "mga.md"),
@@ -90,9 +90,9 @@ populate_empty_chapters!(pages, joinpath(path, "src"))
 
 # Create and deploy the documentation
 makedocs(
-    sitename="SpineOpt.jl",
-    format=Documenter.HTML(prettyurls=get(ENV, "CI", nothing) == "true", size_threshold=409600),  # uncomment to deploy locally
-    pages=pages,
-    warnonly=true
+    sitename = "SpineOpt.jl",
+    format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true", size_threshold = 409600, assets = ["assets/style.css"]),  # uncomment to deploy locally
+    pages = pages,
+    warnonly = true,
 )
-deploydocs(repo="github.com/spine-tools/SpineOpt.jl.git", versions=["stable" => "v^", "v#.#"])
+deploydocs(repo = "github.com/spine-tools/SpineOpt.jl.git", versions = ["stable" => "v^", "v#.#"])
