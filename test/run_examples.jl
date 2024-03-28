14a003994e1302361c0d45c5de46f671152f4fd2
Ni Wang <ni.wang@tno.nl>
2024-03-28 16:53:01 +0100

    Change test

--- test/run_examples.jl
+++ test/run_examples.jl
@@ -12,7 +12,12 @@ objective_function_reference_values = Dict(
         m = run_spineopt(input_data, nothing; log_level=3)        
         @test termination_status(m) == MOI.OPTIMAL
         if haskey(objective_function_reference_values, basename(path))
-            @test abs(objective_value(m) - objective_function_reference_values[basename(path)]) < 1e-4
+            mip_cases = ["6_unit_system.json", "unit_commitment.json"]
+            if  basename(path) in mip_cases    # for mip cases
+                @test abs(objective_value(m) - objective_function_reference_values[basename(path)]) / objective_function_reference_values[basename(path)] ≤ 0.01 
+            else
+                @test abs(objective_value(m) - objective_function_reference_values[basename(path)]) < 1e-4
+            end
         end
     end
 end
