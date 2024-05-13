de939b6d6e9f0ec46526a712ff45536d17e6050c
Manuel Marin <manuel.marin@energyreform.ie>
2024-05-13 07:49:07 +0200

    Update preprocess_data_structure.jl

--- src/data_structure/preprocess_data_structure.jl
+++ src/data_structure/preprocess_data_structure.jl
@@ -958,13 +958,14 @@ function generate_unit_commitment_parameters()
             (x.unit for x in indices(ramp_down_limit)),
             (x.unit for x in indices(units_on_coefficient) if units_on_coefficient(; x...) != 0),
             (x.unit for x in indices(minimum_operating_point) if minimum_operating_point(; x...) != 0),
-            # The variable units_on is by default linear (defined in the template) in cases where it is used.
-            # Moreover, when the user explicitly set this variable to binary or integer, 
-            # we believe the user means to use it as well.
+            # The variable units_on is by default linear.
+            # When the user explicitly set this variable to binary or integer, we assume they mean to use it as well.
             (
-                u for u in indices(online_variable_type) if online_variable_type(unit=u) in [
+                u
+                for u in indices(online_variable_type)
+                if online_variable_type(unit=u) in (
                     :unit_online_variable_type_binary, :unit_online_variable_type_integer
-                ]
+                )
             ),
         )
     )
