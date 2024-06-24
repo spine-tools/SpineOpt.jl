12a8205cc17d1a23c19a726b7781b1484fb2b36a
Niina Helisto <niina.helisto@vtt.fi>
2024-06-24 15:50:35 +0300

    Adding checks when renaming lifetime parameters

--- src/data_structure/versions/rename_lifetime_to_tech_lifetime.jl
+++ src/data_structure/versions/rename_lifetime_to_tech_lifetime.jl
@@ -17,29 +17,35 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #############################################################################
 """
-	rename_lifetime_to_tech_lifetime(db_url, log_level)
+    rename_lifetime_to_tech_lifetime(db_url, log_level)
 
 Replace [xxx]_investment_lifetime by [xxx]_investment_tech_lifetime in all object and relationship class names.
 """
 function rename_lifetime_to_tech_lifetime(db_url, log_level)
-	@log log_level 0 "Renaming `[xxx]_investment_lifetime` to `[xxx]_investment_tech_lifetime`... "
-#    pdef = run_request(db_url, "call_method", ("get_item", "parameter_definition"), Dict(
-#        "entity_class_name" => "connection", "name" => "connection_investment_lifetime")
-#    )
-#    run_request(db_url, "call_method", ("update_item", "parameter_definition"), Dict(
-#        "id" => pdef["id"], "name" => "connection_investment_tech_lifetime")
-#    )
-#    pdef = run_request(db_url, "call_method", ("get_item", "parameter_definition"), Dict(
-#        "entity_class_name" => "node", "name" => "storage_investment_lifetime")
-#    )
-#    run_request(db_url, "call_method", ("update_item", "parameter_definition"), Dict(
-#        "id" => pdef["id"], "name" => "storage_investment_tech_lifetime")
-#    )
-#    pdef = run_request(db_url, "call_method", ("get_item", "parameter_definition"), Dict(
-#        "entity_class_name" => "unit", "name" => "unit_investment_lifetime")
-#    )
-#    run_request(db_url, "call_method", ("update_item", "parameter_definition"), Dict(
-#        "id" => pdef["id"], "name" => "unit_investment_tech_lifetime")
-#    )
-	true
+    @log log_level 0 "Renaming `[xxx]_investment_lifetime` to `[xxx]_investment_tech_lifetime`... "
+    pdef = run_request(db_url, "call_method", ("get_item", "parameter_definition"), Dict(
+        "entity_class_name" => "connection", "name" => "connection_investment_lifetime")
+    )
+    if length(pdef) > 0
+        run_request(db_url, "call_method", ("update_item", "parameter_definition"), Dict(
+            "id" => pdef["id"], "name" => "connection_investment_tech_lifetime")
+        )
+    end
+    pdef = run_request(db_url, "call_method", ("get_item", "parameter_definition"), Dict(
+        "entity_class_name" => "node", "name" => "storage_investment_lifetime")
+    )
+    if length(pdef) > 0
+        run_request(db_url, "call_method", ("update_item", "parameter_definition"), Dict(
+            "id" => pdef["id"], "name" => "storage_investment_tech_lifetime")
+        )
+    end
+    pdef = run_request(db_url, "call_method", ("get_item", "parameter_definition"), Dict(
+        "entity_class_name" => "unit", "name" => "unit_investment_lifetime")
+    )
+    if length(pdef) > 0
+        run_request(db_url, "call_method", ("update_item", "parameter_definition"), Dict(
+            "id" => pdef["id"], "name" => "unit_investment_tech_lifetime")
+        )
+    end
+    true
 end
