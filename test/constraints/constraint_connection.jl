c2516f19788d10ca7f69afaafe9534124942f922
datejada <diego.tejadaarango@tno.nl>
2024-06-28 11:01:43 +0200

    Update tests to the ones in master branch

--- test/constraints/constraint_connection.jl
+++ test/constraints/constraint_connection.jl
@@ -542,51 +542,6 @@ function test_constraint_node_voltage_angle()
     end
 end
 
-function test_constraint_connection_flow_capacity_investments()
-    @testset "constraint_connection_flow_capacity_investments" begin
-        url_in = _test_constraint_connection_setup()
-        connection_capacity = 200
-        object_parameter_values = [
-            ["connection", "connection_ab", "candidate_connections", 1],
-            [
-                "connection",
-                "connection_ab",
-                "connection_investment_tech_lifetime",
-                Dict("type" => "duration", "data" => "60m"),
-            ],
-        ]
-        relationships = [
-            ["connection__investment_temporal_block", ["connection_ab", "hourly"]],
-            ["connection__investment_stochastic_structure", ["connection_ab", "stochastic"]],
-        ]
-        relationship_parameter_values =
-            [["connection__from_node", ["connection_ab", "node_a"], "connection_capacity", connection_capacity]]
-        SpineInterface.import_data(
-            url_in;
-            object_parameter_values=object_parameter_values,
-            relationships=relationships,
-            relationship_parameter_values=relationship_parameter_values,
-        )
-        m = run_spineopt(url_in; log_level=0, optimize=false)
-        var_connection_flow = m.ext[:spineopt].variables[:connection_flow]
-        var_connections_invested_available = m.ext[:spineopt].variables[:connections_invested_available]
-        constraint = m.ext[:spineopt].constraints[:connection_flow_capacity]
-        @test length(constraint) == 2
-        scenarios = (stochastic_scenario(:parent), stochastic_scenario(:child))
-        time_slices = time_slice(m; temporal_block=temporal_block(:hourly))
-        @testset for (s, t) in zip(scenarios, time_slices)
-            key = (connection(:connection_ab), node(:node_a), direction(:from_node), s, t)
-            invest_key = (connection(:connection_ab), s, t)
-            var_conn_flow = var_connection_flow[key...]
-            var_conn_inv_a = var_connections_invested_available[invest_key...]
-            expected_con = @build_constraint(var_conn_flow <= connection_capacity * var_conn_inv_a)
-            con_key = (connection(:connection_ab), node(:node_a), direction(:from_node), [s], t)
-            observed_con = constraint_object(constraint[con_key...])
-            @test _is_constraint_equal(observed_con, expected_con)
-        end
-    end
-end
-
 function test_constraint_connection_intact_flow_ptdf()
     @testset "constraint_connection_intact_flow_ptdf" begin
         url_in = _test_constraint_connection_setup()
