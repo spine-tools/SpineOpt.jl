fa597aa01d1616127254dc17a2de515c8e07b76a
Manuel <chacarero@gmail.com>
2024-07-03 11:08:43 +0200

    Fix typo

--- src/data_structure/temporal_structure.jl
+++ src/data_structure/temporal_structure.jl
@@ -410,7 +410,7 @@ end
 function generate_time_slice!(m::Model)
     _generate_as_number_or_call!(m)
     _generate_time_slice!(m)
-    _generate_output_time_slices!(m)
+    _generate_output_time_slice!(m)
     _generate_time_slice_relationships!(m)
 end
 
