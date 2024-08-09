a4d7b263416a559be0bceccd3cb4dc4b95926001
Diego Alejandro Tejada Arango <12887482+datejada@users.noreply.github.com>
2024-08-09 15:41:02 +0200

    Add economic parameter results to the outputs (#1053)

--- src/run_spineopt_basic.jl
+++ src/run_spineopt_basic.jl
@@ -607,6 +607,7 @@ function _save_model_results!(m)
     _save_constraint_values!(m)
     _save_objective_values!(m)
     _save_other_values!(m)
+    _save_economic_parameter_values!(m)
 end
 
 """
@@ -633,6 +634,123 @@ function _save_other_values!(m::Model)
     catch err
         @warn err
     end
+end
+
+"""
+Generalized function to initialize and populate economic parameter values.
+"""
+function _initialize_and_populate_values!(
+    m::Model,
+    param_name::Symbol,
+    indices_func,
+    value_func,
+    entity::Symbol,
+    user_outputs
+)
+    
+    key_type = NamedTuple{(
+        entity, :stochastic_scenario, :t),
+        Tuple{SpineInterface.Object, SpineInterface.Object, SpineInterface.TimeSlice}
+    }
+    
+    if !use_economic_representation(model=m.ext[:spineopt].instance)
+        m.ext[:spineopt].values[param_name] = Dict{key_type,Float64}()
+        return nothing
+    end
+
+    m.ext[:spineopt].values[param_name] = 
+        Dict{key_type,Float64}(
+            (entity=>e, stochastic_scenario=s, t=t) =>
+            value_func(; entity=>e, stochastic_scenario=s, t=t)
+            for (e, s, t) in indices_func(m) if param_name in user_outputs
+        )
+    return nothing
+end
+
+"""
+Save the value of the economic parameters of units, connections, and storages if the user wants to report it.
+"""
+function _save_economic_parameter_values!(m::Model)
+    user_outputs = (out_name for (out_name, _ow) in keys(m.ext[:spineopt].reports_by_output))
+
+    # Units
+    _initialize_and_populate_values!(
+        m,
+        :unit_salvage_fraction,
+        units_invested_available_indices,
+        unit_salvage_fraction,
+        :unit,
+        user_outputs
+    )
+    _initialize_and_populate_values!(
+        m,
+        :unit_tech_discount_factor,
+        units_invested_available_indices,
+        unit_tech_discount_factor,
+        :unit,
+        user_outputs
+    )
+    _initialize_and_populate_values!(
+        m,
+        :unit_conversion_to_discounted_annuities,
+        units_invested_available_indices,
+        unit_conversion_to_discounted_annuities,
+        :unit,
+        user_outputs
+    )
+
+    # Connections
+    _initialize_and_populate_values!(
+        m,
+        :connection_salvage_fraction,
+        connections_invested_available_indices,
+        connection_salvage_fraction,
+        :connection,
+        user_outputs
+    )
+    _initialize_and_populate_values!(
+        m,
+        :connection_tech_discount_factor,
+        connections_invested_available_indices,
+        connection_tech_discount_factor,
+        :connection,
+        user_outputs
+    )
+    _initialize_and_populate_values!(
+        m,
+        :connection_conversion_to_discounted_annuities,
+        connections_invested_available_indices,
+        connection_conversion_to_discounted_annuities,
+        :connection,
+        user_outputs
+    )
+
+    # Storages
+    _initialize_and_populate_values!(
+        m,
+        :storage_salvage_fraction,
+        storages_invested_available_indices,
+        storage_salvage_fraction,
+        :node,
+        user_outputs
+    )
+    _initialize_and_populate_values!(
+        m,
+        :storage_tech_discount_factor,
+        storages_invested_available_indices,
+        storage_tech_discount_factor,
+        :node,
+        user_outputs
+    )
+    _initialize_and_populate_values!(
+        m,
+        :storage_conversion_to_discounted_annuities,
+        storages_invested_available_indices,
+        storage_conversion_to_discounted_annuities,
+        :node,
+        user_outputs
+    )
+    return nothing
 end
 
 """
