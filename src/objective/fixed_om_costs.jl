19acae2a2d21ad57312a5934d8c74f3029696167
nnhjy <jiangyi.huang@outlook.com>
2024-10-11 15:38:51 +0200

    use unit_flow_indices in fom_cost

--- src/objective/fixed_om_costs.jl
+++ src/objective/fixed_om_costs.jl
@@ -28,20 +28,25 @@ function fixed_om_costs(m, t_range)
         m,
         sum(
             + unit_capacity(m; unit=u, node=ng, direction=d, stochastic_scenario=s, t=t)
-            * (use_economic_representation(model=m.ext[:spineopt].instance) ?
-               unit_discounted_duration[(unit=u, stochastic_scenario=s, t=t)] : 1
+            * (
+                use_economic_representation(model=m.ext[:spineopt].instance) ? 
+                unit_discounted_duration[(unit=u, stochastic_scenario=s, t=t)] : 1
             ) 
             * fom_cost(m; unit=u, stochastic_scenario=s, t=t)
             * (
                 + number_of_units(m; unit=u, stochastic_scenario=s, t=t)
-                + units_invested_available[u, s, t]
+                + (
+                    u in intersect(indices(candidate_units), members(u)) ? 
+                    units_invested_available[u, s, t] : 0
+                )
             )
             * prod(weight(temporal_block=blk) for blk in blocks(t))
             # This term is activated when there is a representative temporal block that includes t.
             # We assume only one representative temporal structure available, of which the termporal blocks represent
             # an extended period of time with a weight >=1, e.g. a representative month represents 3 months.
-            * duration(t) for (u, ng, d) in indices(unit_capacity; unit=indices(fom_cost))
-            for (u, s, t) in units_invested_available_indices(m; unit=u, t=t_range);
+            * duration(t) 
+            for (u, ng, d) in indices(unit_capacity; unit=indices(fom_cost))
+            for (u, _n, _d, s, t) in unit_flow_indices(m; unit=u, node=ng, direction=d, t=t_range);
             init=0,
         )
     )
