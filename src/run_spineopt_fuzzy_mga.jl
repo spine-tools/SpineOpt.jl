6557790c3897bd043b2ecdafca36958110f8cfef
Kacper Kozerski <kozerskikacper@gmail.com>
2025-01-26 12:27:49 +0100

    general rework of the MGA, needed that to create a basis for further method expansion

--- src/run_spineopt_fuzzy_mga.jl
+++ src/run_spineopt_fuzzy_mga.jl
@@ -17,6 +17,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #############################################################################
 
+import DataStructures: DefaultDict
+
 function do_run_spineopt!(
     m,
     url_out,
@@ -28,8 +30,6 @@ function do_run_spineopt!(
     write_as_roll,
     resume_file_path,
 )
-    outputs = Dict()
-    mga_iteration_count = 0
     max_mga_iters = max_mga_iterations(model=m.ext[:spineopt].instance)
     mga_iteration = ObjectClass(:mga_iteration, [])
     @eval mga_iteration = $mga_iteration
@@ -38,209 +38,167 @@ function do_run_spineopt!(
         m;
         log_level=log_level,
         update_names=update_names,
-        output_suffix=_add_mga_iteration(mga_iteration_count),
+        output_suffix=_add_mga_iteration(0),
     )
-    mga_iteration_count += 1
-    add_mga_objective_constraint!(m)
-    set_mga_objective!(m)
-    iter_weights = Dict()
-    while mga_iteration_count <= max_mga_iters
-        set_objective_mga_iteration!(m, iter_weights; iteration=last(mga_iteration()), iteration_number=mga_iteration_count)
+    variable_group_parameters = Dict(
+        :units_invested  => (
+            units_invested_available_indices,
+            unit_stochastic_scenario_weight,
+            units_invested_mga_indices,
+        ),
+        :connections_invested  => (
+            connections_invested_available_indices,
+            connection_stochastic_scenario_weight,
+            connections_invested_mga_indices,
+        ),
+        :storages_invested  => (
+            storages_invested_available_indices,
+            node_stochastic_scenario_weight,
+            storages_invested_mga_indices,
+        ),
+    )
+    variable_groups = keys(variable_group_parameters)
+    hsj_weights = update_hsj_weights!(m, last(mga_iteration()), nothing, variable_group_parameters)
+    add_fuzzy_mga_objective_constraint!(m)
+    for i=1:max_mga_iters
+        prepare_objective_fuzzy_mga!(m, hsj_weights, last(mga_iteration()), variable_group_parameters)
+        set_fuzzy_mga_objective!(m, variable_groups)
         solve_model!(
             m;
             log_level=log_level,
             update_names=update_names,
-            output_suffix=_add_mga_iteration(mga_iteration_count),
+            output_suffix=_add_mga_iteration(i),
         ) || break
-        save_mga_objective_values!(m)
-        if mga_iteration_count < max_mga_iters
-            for name in (:mga_objective_ub, :mga_diff_ub1)
-                for con in values(m.ext[:spineopt].constraints[name])
-                    try
-                        delete(m, con)
-                    catch
-                    end
-                end
-            end
-        end
-        # TODO: needs to clean constraint (or simply clean within function)
-        mga_iteration_count += 1
+        update_hsj_weights!(m, last(mga_iteration()), hsj_weights, variable_group_parameters)
     end
     write_report(m, url_out; alternative=alternative, log_level=log_level)
     m
 end
 
-function _add_mga_iteration(k)
-    new_mga_name = Symbol(:mga_it_, k)
-    new_mga_i = Object(new_mga_name, :mga_iteration)
-    add_object!(mga_iteration, new_mga_i)
-    (mga_iteration=mga_iteration(new_mga_name),)
-end
-
-function units_invested_mga_indices()
-    unique((unit=ug,) for ug in unit(units_invested_mga=true))
-end
-
-function units_invested_mga_indices(mga_iteration)
-    unique((unit=ug, mga_iteration=mga_it) for ug in unit(units_invested_mga=true) for mga_it in mga_iteration)
-end
-
-function connections_invested_mga_indices()
-    unique((connection=cg,) for cg in connection(connections_invested_mga=true))
-end
-
-function connections_invested_mga_indices(mga_iteration)
-    unique(
-        (connection=cg, mga_iteration=mga_it)
-        for cg in connection(connections_invested_mga=true)
-        for mga_it in mga_iteration
-    )
-end
-
-function storages_invested_mga_indices()
-    unique((node=ng,) for ng in node(storages_invested_mga=true))
-end
-
-function storages_invested_mga_indices(mga_iteration)
-    unique(
-        (node=ng, mga_iteration=mga_it)
-        for ng in node(storages_invested_mga=true)
-        for mga_it in mga_iteration
-    )
-end
-
-function set_objective_mga_iteration!(m, iter_weights; iteration=nothing, iteration_number=0)
-    instance = m.ext[:spineopt].instance
-    _set_objective_mga_iteration!(
-        m,
-        :units_invested,
-        units_invested_available_indices,
-        unit_stochastic_scenario_weight,
-        units_invested_mga_indices,
-        iter_weights,
-        iteration,
-        iteration_number
-    )
-    _set_objective_mga_iteration!(
-        m,
-        :connections_invested,
-        connections_invested_available_indices,
-        connection_stochastic_scenario_weight,
-        connections_invested_mga_indices,
-        iter_weights,
-        iteration,
-        iteration_number
-    )
-    _set_objective_mga_iteration!(
-        m,
-        :storages_invested,
-        storages_invested_available_indices,
-        node_stochastic_scenario_weight,
-        storages_invested_mga_indices,
-        iter_weights,
-        iteration,
-        iteration_number
-    )
-    @fetch mga_aux_diff, mga_objective = m.ext[:spineopt].variables
-    ub_objective = get!(m.ext[:spineopt].constraints, :mga_objective_ub, Dict())
-    ub_objective[(model=m.ext[:spineopt].instance,)] = @constraint(
-        m,
-        mga_objective[(model = m.ext[:spineopt].instance, t=current_window(m))]
-        >= sum(
-            mga_aux_diff[ind]
-            for ind in vcat(
-                storages_invested_mga_indices(iteration),
-                connections_invested_mga_indices(iteration),
-                units_invested_mga_indices(iteration)
-            )
+function prepare_objective_fuzzy_mga!(m, hsj_weights, iteration, group_parameters)
+    for (group_name, (available_indices, scenario_weights, mga_indices)) in group_parameters
+        do_prepare_objective_fuzzy_mga!(
+            m,
+            group_name,
+            available_indices,
+            scenario_weights,
+            mga_indices,
+            hsj_weights[group_name],
+            iteration
         )
-    )
-    _update_constraint_names!(m)
+    end
 end
 
-function _set_objective_mga_iteration!(
+function do_prepare_objective_fuzzy_mga!(
     m::Model,
     variable_name::Symbol,
     variable_indices_function::Function,
     scenario_weight_function::Function,
     mga_indices::Function,
-    iter_weights::Dict,
-    mga_current_iteration::Object,
-    iteration_number::Int64,
-)
-    if !isempty(mga_indices())
-        t0 = start(current_window(m))
-        @fetch units_invested = m.ext[:spineopt].variables
-        d_aux = get!(m.ext[:spineopt].variables, :mga_aux_diff, Dict())
-        for ind in mga_indices(mga_current_iteration)
-            d_aux[ind] = @variable(m, base_name=_base_name(:mga_aux_diff, ind))
-        end
-        @fetch mga_aux_diff = m.ext[:spineopt].variables
-        variable = m.ext[:spineopt].variables[variable_name]
-        d_diff_ub1 = get!(m.ext[:spineopt].constraints, :mga_diff_ub1, Dict())
-        for ind in mga_indices()
-            if sum(
-                value(variable[ind_]) * scenario_weight_function(m; _drop_key(ind_, :t)...)
-                for ind_ in variable_indices_function(m; ind...)
-                ) > 0
-                iter_weights[variable_name, ind] = 1
-            end
-            
-            # for ind_ in variable_indices_function(m; ind...) end  # ???!!!
-            d_diff_ub1[(ind..., mga_current_iteration...)] = @constraint(
-                m,
-                mga_aux_diff[(ind..., mga_iteration=mga_current_iteration)]
-                ==
-                (
-                    sum(
-                        variable[ind_] * scenario_weight_function(m; _drop_key(ind_, :t)...)
-                        for ind_ in variable_indices_function(m; ind...)
-                    )
-                )
-                * get!(iter_weights, (variable_name, ind), 0)
-            )
-        end
-    end
-end
-
-function _mga_result(m, variable_name, ind, mga_iteration)
-    suffix = (mga_iteration=mga_iteration,)
-    window = start(current_window(m)), end_(current_window(m))
-    m.ext[:spineopt].outputs[variable_name][suffix][window][_static(ind)]
-end
-
-function add_mga_objective_constraint!(m::Model)
+    iter_weights::DefaultDict,
+    mga_current_iteration::Object
+)   
     instance = m.ext[:spineopt].instance
-    m.ext[:spineopt].constraints[:mga_slack_constraint] = Dict(
-        (model=m.ext[:spineopt].instance,) => @constraint(
+    t = current_window(m)
+    objective_variables = get!(m.ext[:spineopt].expressions, :mga_objective, Dict())
+    weighted_mga_variables = get!(objective_variables, (model=instance, t=t), Dict())
+    @fetch units_invested = m.ext[:spineopt].variables
+    if !isempty(mga_indices())
+        weighted_mga_variables[variable_name] = @expression(
             m,
-            total_costs(m, anything)
-            <=
-            (1 + max_mga_slack(model=instance)) * objective_value(m)
+            sum(
+                get_scenario_variable_average(m, variable_name, ind, variable_indices_function, scenario_weight_function) * iter_weights[(variable_name, ind)]
+                for ind in mga_indices()
+            )        
         )
-    )
-end
-
-function save_mga_objective_values!(m::Model)
-    ind = (model=m.ext[:spineopt].instance, t=current_window(m))
-    for name in [:mga_objective,]
-        for ind in keys(m.ext[:spineopt].variables[name])
-            m.ext[:spineopt].values[name] = Dict(ind => value(m.ext[:spineopt].variables[name][ind]))
-        end
     end
 end
 
-function set_mga_objective!(m)
-    m.ext[:spineopt].variables[:mga_objective] = Dict(
-        (model=m.ext[:spineopt].instance, t=current_window(m)) => @variable(
-            m,
-            base_name=_base_name(:mga_objective, (model=m.ext[:spineopt].instance, t=current_window(m))),
-            lower_bound=Inf
+function add_fuzzy_mga_objective_constraint!(m::Model)
+    instance = m.ext[:spineopt].instance
+    slack = objective_value(m) >= 0 ? max_mga_slack(model=instance) : -max_mga_slack(model=instance)
+    m.ext[:spineopt].constraints[:mga_slack_constraint] = Dict(
+        (instance,) => @constraint(m,
+            total_costs(m, anything) <= (1 + slack) * objective_value(m)
         )
     )
+end
+
+function set_fuzzy_mga_objective!(m, variable_groups)
+    instance = model=m.ext[:spineopt].instance
+    t = current_window(m)
+    weighted_mga_variables = m.ext[:spineopt].expressions[:mga_objective][(model=instance, t=t)]
     @objective(
         m,
         Min,
-        m.ext[:spineopt].variables[:mga_objective][(model=m.ext[:spineopt].instance, t=current_window(m))]
+        sum(
+            get(weighted_mga_variables, mga_group, 0) for mga_group in variable_groups
+        )
     )
+end
+
+function get_scenario_variable_average(
+    m::Model,
+    variable_name::Symbol,
+    variable_indicator,
+    variable_indices_function::Function,
+    scenario_weight_function::Function
+)
+    variable = m.ext[:spineopt].variables[variable_name]
+    return sum(
+        variable[ind] * scenario_weight_function(m; _drop_key(ind, :t)...)
+        for ind in variable_indices_function(m; variable_indicator...)
+    )
+end
+
+function was_variable_active(
+    m::Model,
+    variable_name::Symbol,
+    variable_indices_function::Function,
+    variable_indicator
+)
+    variable = m.ext[:spineopt].variables[variable_name]
+    return any(
+        value(variable[ind]) > 0
+        for ind in variable_indices_function(m; variable_indicator...)
+    )
+end
+
+function update_hsj_weights!(
+    m::Model,
+    mga_current_iteration,
+    variable_hsj_weights,
+    group_parameters
+)
+    if isnothing(variable_hsj_weights)
+        variable_hsj_weights = Dict(
+            group => DefaultDict(0) for group in keys(group_parameters)
+        )
+    end
+    for (group_name, (available_indices, _, mga_indices)) in group_parameters
+        do_update_hsj_weights!(
+            m,
+            group_name,
+            available_indices,
+            mga_indices,
+            mga_current_iteration,
+            variable_hsj_weights[group_name],
+        )
+    end
+    return variable_hsj_weights
+end
+
+function do_update_hsj_weights!(
+    m::Model,
+    variable_name::Symbol,
+    variable_indices_function::Function,
+    mga_indices::Function,
+    mga_current_iteration::Object,
+    variable_hsj_weights::DefaultDict
+)
+    for ind in mga_indices()
+        if was_variable_active(m, variable_name, variable_indices_function, ind)
+            variable_hsj_weights[variable_name, ind] = 1
+        end
+    end
 end
---
+++ test/run_spineopt_fuzzy_mga.jl
@@ -0,0 +1,169 @@
+function _test_run_spineopt_mga_setup()
+    url_in = "sqlite://"
+    test_data = Dict(
+        :objects => [
+            ["model", "instance"],
+            ["temporal_block", "hourly"],
+            ["temporal_block", "investments_hourly"],
+            ["temporal_block", "two_hourly"],
+            ["stochastic_structure", "deterministic"],
+            ["stochastic_structure", "investments_deterministic"],
+            ["stochastic_structure", "stochastic"],
+            ["unit", "unit_ab"],
+            ["unit", "unit_bc"],
+            ["unit", "unit_group_abbc"],
+            ["node", "node_a"],
+            ["node", "node_b"],
+            ["node", "node_c"],
+            ["node", "node_group_bc"],
+            ["connection", "connection_ab"],
+            ["connection", "connection_bc"],
+            ["connection", "connection_group_abbc"],
+            ["stochastic_scenario", "parent"],
+            ["stochastic_scenario", "child"],
+            # FIXME: maybe nicer way rather than outputs?
+            ["output","units_invested"],
+            ["output","connections_invested"],
+            ["output","storages_invested"],
+            ["output","total_costs"],
+            ["report", "report_a"]
+        ],
+        :object_groups => [
+                ["node", "node_group_bc", "node_b"],
+                ["node", "node_group_bc", "node_c"],
+                ["connection", "connection_group_abbc", "connection_ab"],
+                ["connection", "connection_group_abbc", "connection_bc"],
+                ["unit", "unit_group_abbc", "unit_ab"],
+                ["unit", "unit_group_abbc", "unit_bc"],
+                ],
+        :relationships => [
+            ["model__temporal_block", ["instance", "hourly"]],
+            ["model__temporal_block", ["instance", "two_hourly"]],
+            ["model__default_investment_temporal_block", ["instance", "two_hourly"]],
+            ["model__stochastic_structure", ["instance", "deterministic"]],
+            ["model__stochastic_structure", ["instance", "stochastic"]],
+            ["model__default_investment_stochastic_structure", ["instance", "deterministic"]],
+            ["connection__from_node", ["connection_ab", "node_a"]],
+            ["connection__to_node", ["connection_ab", "node_b"]],
+            ["connection__from_node", ["connection_bc", "node_b"]],
+            ["connection__to_node", ["connection_bc", "node_c"]],
+            ["node__temporal_block", ["node_a", "hourly"]],
+            ["node__temporal_block", ["node_b", "two_hourly"]],
+            ["node__temporal_block", ["node_c", "hourly"]],
+            ["node__stochastic_structure", ["node_a", "stochastic"]],
+            ["node__stochastic_structure", ["node_b", "deterministic"]],
+            ["node__stochastic_structure", ["node_c", "stochastic"]],
+            ["stochastic_structure__stochastic_scenario", ["deterministic", "parent"]],
+            ["stochastic_structure__stochastic_scenario", ["stochastic", "parent"]],
+            ["stochastic_structure__stochastic_scenario", ["stochastic", "child"]],
+            ["stochastic_structure__stochastic_scenario", ["investments_deterministic", "parent"]],
+            ["parent_stochastic_scenario__child_stochastic_scenario", ["parent", "child"]],
+            ["units_on__temporal_block", ["unit_ab", "hourly"]],
+            ["units_on__temporal_block", ["unit_bc", "hourly"]],
+            ["units_on__stochastic_structure", ["unit_ab", "stochastic"]],
+            ["units_on__stochastic_structure", ["unit_bc", "stochastic"]],
+            ["unit__from_node", ["unit_ab", "node_a"]],
+            ["unit__from_node", ["unit_bc", "node_b"]],
+            ["unit__to_node", ["unit_ab", "node_b"]],
+            ["unit__to_node", ["unit_bc", "node_c"]],
+            ["report__output",["report_a", "units_invested"]],
+            ["report__output",["report_a","connections_invested"]],
+            ["report__output",["report_a","storages_invested"]],
+            ["report__output",["report_a","total_costs"]],
+            ["model__report",["instance","report_a"]],
+            ["unit__node__node", ["unit_ab", "node_a", "node_b"]],
+            ["connection__node__node", ["connection_ab", "node_a", "node_b"]],
+            ["unit__node__node", ["unit_ab", "node_b", "node_a"]],
+            ["connection__node__node", ["connection_ab", "node_b", "node_a"]],
+            ["unit__node__node", ["unit_bc", "node_b", "node_c"]],
+            ["connection__node__node", ["connection_bc", "node_b", "node_c"]],
+            ["unit__node__node", ["unit_bc", "node_c", "node_b"]],
+            ["connection__node__node", ["connection_bc", "node_c", "node_b"]],
+        ],
+        :object_parameter_values => [
+            ["model", "instance", "model_start", Dict("type" => "date_time", "data" => "2000-01-01T00:00:00")],
+            ["model", "instance", "model_end", Dict("type" => "date_time", "data" => "2000-01-01T02:00:00")],
+            ["model", "instance", "duration_unit", "hour"],
+            ["model", "instance", "model_algorithm", "fuzzy_mga_algorithm"],
+            ["temporal_block", "hourly", "resolution", Dict("type" => "duration", "data" => "1h")],
+            ["temporal_block", "two_hourly", "resolution", Dict("type" => "duration", "data" => "2h")],
+        ],
+        :relationship_parameter_values => [
+            [
+                "stochastic_structure__stochastic_scenario",
+                ["stochastic", "parent"],
+                "stochastic_scenario_end",
+                Dict("type" => "duration", "data" => "1h")
+            ],
+            ["connection__node__node", ["connection_ab", "node_b", "node_a"], "fix_ratio_out_in_connection_flow", 1.0],
+            ["connection__node__node", ["connection_ab", "node_a", "node_b"], "fix_ratio_out_in_connection_flow", 1.0],
+            ["connection__node__node", ["connection_bc", "node_c", "node_b"], "fix_ratio_out_in_connection_flow", 1.0],
+            ["connection__node__node", ["connection_bc", "node_b", "node_c"], "fix_ratio_out_in_connection_flow", 1.0],
+            ["unit__node__node", ["unit_ab", "node_b", "node_a"], "fix_ratio_out_in_unit_flow", 1.0],
+            ["unit__node__node", ["unit_ab", "node_a", "node_b"], "fix_ratio_out_in_unit_flow", 1.0],
+            ["unit__node__node", ["unit_bc", "node_c", "node_b"], "fix_ratio_out_in_unit_flow", 1.0],
+            ["unit__node__node", ["unit_bc", "node_b", "node_c"], "fix_ratio_out_in_unit_flow", 1.0],
+
+        ],
+    )
+    _load_test_data(url_in, test_data)
+    url_in
+end
+
+function _test_run_fuzzy_spineopt_mga()
+    @testset "run_spineopt_fuzzy_mga" begin
+        url_in = _test_run_spineopt_mga_setup()
+        candidate_units = 1
+        candidate_connections = 1
+        candidate_storages = 1
+        fuel_cost = 5
+        mga_slack = 0.05
+        object_parameter_values = [
+            ["unit", "unit_ab", "candidate_units", candidate_units],
+            ["unit", "unit_bc", "candidate_units", candidate_units],
+            ["unit", "unit_ab", "number_of_units", 0],
+            ["unit", "unit_bc", "number_of_units", 0],
+            ["unit", "unit_group_abbc", "units_invested_mga", true],
+            ["unit", "unit_group_abbc", "units_invested__mga_weight", 1],
+            ["unit", "unit_ab", "unit_investment_cost", 1],
+            ["connection", "connection_ab", "candidate_connections", candidate_connections],
+            ["connection", "connection_bc", "candidate_connections", candidate_connections],
+            ["connection", "connection_group_abbc", "connections_invested_mga", true],
+            ["connection", "connection_group_abbc", "connections_invested_mga_weight", 1],
+            ["node", "node_b", "candidate_storages", candidate_storages],
+            ["node", "node_c", "candidate_storages", candidate_storages],
+            ["node", "node_a", "balance_type", :balance_type_none],
+            ["node", "node_b", "has_state", true],
+            ["node", "node_c", "has_state", true],
+            ["node", "node_b", "fix_node_state",0],
+            ["node", "node_c", "fix_node_state",0],
+            ["node", "node_b", "node_state_cap", 0],
+            ["node", "node_c", "node_state_cap", 0],
+            ["node", "node_group_bc", "storages_invested_mga", true],
+            ["node", "node_group_bc", "storages_invested_mga_weight", 1],
+            ["model", "instance", "model_algorithm", "fuzzy_mga_algorithm"],
+            ["model", "instance", "max_mga_slack", mga_slack],
+            ["model", "instance", "max_mga_iterations", 2],
+            # ["node", "node_a", "demand", 1],
+            ["node", "node_b", "demand", 1],
+            ["node", "node_c", "demand", 1],
+        ]
+        relationship_parameter_values = [
+            ["unit__to_node", ["unit_ab", "node_b"], "unit_capacity", 5],
+            ["unit__to_node", ["unit_ab", "node_b"], "fuel_cost", fuel_cost],
+            ["unit__to_node", ["unit_bc", "node_c"], "unit_capacity", 5],
+            ["connection__to_node", ["connection_ab","node_b"], "connection_capacity", 5],
+            ["connection__to_node", ["connection_bc","node_c"], "connection_capacity", 5]
+        ]
+        SpineInterface.import_data(
+            url_in;
+            object_parameter_values=object_parameter_values,
+            relationship_parameter_values=relationship_parameter_values
+        )
+        m = run_spineopt(url_in; log_level=1, add_bridges=true)
+    end
+end
+
+@testset "run_spineopt_fuzzy_mga" begin
+    _test_run_fuzzy_spineopt_mga()
+end
