6a51c7973ad271d71cbf9bed9bfce7b49e41f542
Niina Helisto <niina.helisto@vtt.fi>
2025-02-10 14:37:55 +0200

    Rename node:storage_fom_cost

--- src/data_structure/versions/major_upgrade_to_16.jl
+++ src/data_structure/versions/major_upgrade_to_16.jl
@@ -76,6 +76,7 @@ function major_upgrade_to_16(db_url, log_level)
 		(("node", "frac_state_loss"), "storage_self_discharge", ""),
 		(("node", "number_of_storages"), "existing_storages", ""),
 		(("node", "state_coeff"), "storage_state_coefficient", ""),
+		(("node", "storage_fom_cost"), "storage_fixed_annual_cost", ""),
     ]
     #@log log_level 0 string("Creating superclasses...")
     #@log log_level 0 string("Note: Check entity alternatives in classes related to the unit_flow superclass...")
