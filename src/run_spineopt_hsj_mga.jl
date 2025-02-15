32bcd1f4d40d5e28de560cb8128316f5adb044ea
Kacper Kozerski <kozerskikacper@gmail.com>
2025-02-15 17:39:58 +0100

    changing updating mga objective for more simplicity

--- src/run_spineopt_hsj_mga.jl
+++ src/run_spineopt_hsj_mga.jl
@@ -42,6 +42,7 @@ function do_run_spineopt!(
         output_suffix=_add_mga_iteration(0),
     )
     variables = m.ext[:spineopt].variables
+    m.ext[:spineopt].expressions[:mga_objective_parts] = init_mga_objective_expressions()
     hsj_weights = init_hsj_weights()
     update_hsj_weights!(get_variable_group_values(variables, variable_group_parameters), last(mga_iteration()), hsj_weights, variable_group_parameters)
     add_hsj_mga_objective_constraint!(m)
@@ -87,43 +88,42 @@ function get_variable_group_values(variables, variable_group_parameters)
     )
 end
 
+function init_mga_objective_expressions()
+    return DefaultDict(() -> DefaultDict(0))
+end
+
 function update_hsj_mga_objective!(m, hsj_weights, iteration, group_parameters)
+    instance = m.ext[:spineopt].instance
+    t = current_window(m)
+    mga_weighted_groups = m.ext[:spineopt].expressions[:mga_objective_parts][(model=instance, t=t)]
     for (group_name, (available_indices, scenario_weights, mga_indices_func)) in group_parameters
-        prepare_objective_hsj_mga!(
+        mga_weighted_groups[variable_name] = prepare_objective_hsj_mga!(
             m,
-            group_name,
+            m.ext[:spineopt].variables[group_name],
             available_indices,
             scenario_weights,
             mga_indices_func(),
-            hsj_weights[group_name],
-            iteration
+            hsj_weights[group_name]
         )
     end
-    mga_objective_parts = m.ext[:spineopt].expressions[:mga_objective]
-    mga_weighted_group = mga_objective_parts[(model=m.ext[:spineopt].instance, t=current_window(m))]
-    @objective(
-        m, Min, sum(mga_weighted_group[mga_group] for mga_group in keys(group_parameters))
+    return @objective(
+        m, Min, sum(mga_weighted_groups[mga_group] for mga_group in keys(group_parameters))
     )
 end
 
 function prepare_objective_hsj_mga!(
     m::Model,
-    variable_name::Symbol,
+    variable,
     variable_indices::Function,
     variable_stochastic_weights::Function,
     mga_indices::AbstractArray,
     mga_weights::DefaultDict,
-    mga_current_iteration::Object
 )   
-    instance = m.ext[:spineopt].instance
-    variable = m.ext[:spineopt].variables[variable_name]
-    objective_variables = get!(m.ext[:spineopt].expressions, :mga_objective, Dict())
-    mga_weighted_group = get!(objective_variables, (model=instance, t=current_window(m)), Dict())
     weighted_group_variables = (
         get_scenario_variable_average(variable, variable_indices(i), variable_stochastic_weights) * mga_weights[i]
         for i in mga_indices
     )
-    mga_weighted_group[variable_name] = @expression(m, sum(weighted_group_variables, init=0))
+    return @expression(m, sum(weighted_group_variables, init=0))
 end
 
 function get_scenario_variable_average(variable, variable_indices, scenario_weights)
