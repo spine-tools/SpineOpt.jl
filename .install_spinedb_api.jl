50cb7399877d248d03dc6cc5e827af1df4990b3d
Tars Verschelde <tars.verschelde@kulueven.be>
2025-02-28 14:03:10 +0100

    spinedbapi 8538a199ac9b6a2f036054b426a25efb2cee8e65

--- .install_spinedb_api.jl
+++ .install_spinedb_api.jl
@@ -5,4 +5,4 @@ pkg"add PyCall"
 using PyCall
 python = PyCall.pyprogramname
 run(`$python -m pip install --user setuptools-scm`)
-run(`$python -m pip install --user git+https://github.com/spine-tools/Spine-Database-API`)
+run(`$python -m pip install --user git+https://github.com/spine-tools/Spine-Database-API@8538a199ac9b6a2f036054b426a25efb2cee8e65`)
