c12bc215b0549fc259b91f4f289f3b103f17e30c
Tars Verschelde <tars.verschelde@kulueven.be>
2025-03-03 10:35:49 +0100

    bugfix initial invested available

--- src/variables/variable_connections_invested_available.jl
+++ src/variables/variable_connections_invested_available.jl
@@ -63,7 +63,7 @@ function add_variable_connections_invested_available!(m::Model)
         lb=constant(0),
         int=connections_invested_available_int,
         fix_value=fix_connections_invested_available,
-        initial_value=initial_connections_invested_available,
+        initial_value=_initial_connections_invested_available,
         required_history_period=maximum_parameter_value(connection_investment_tech_lifetime),
     )
 end
