b3c7ad5ebbfdba25c2d7ad5f6803a57e0a07f97f
Diego Alejandro Tejada Arango <diego.tejadaarango@tno.nl>
2025-05-14 16:34:13 +0200

    Update indices calculation of the constraint

--- src/constraints/constraint_connection_min_flow.jl
+++ src/constraints/constraint_connection_min_flow.jl
@@ -111,7 +111,7 @@ end
 function constraint_connection_min_flow_indices(m::Model)    
     (
         (connection=conn, node=ng, direction=d, stochastic_path=path, t=t)
-        for (conn, ng, d) in _connection_node_direction(m)
+        for (conn, ng, d) in _connection_node_direction_for_min_flow(m)
         if members(ng) != [ng] || 
            is_candidate(connection=conn)
         for (t, path) in t_lowest_resolution_path(
@@ -119,6 +119,36 @@ function constraint_connection_min_flow_indices(m::Model)
             connection_flow_indices(m; connection=conn, node=ng, direction=d),
             connections_invested_available_indices(m; connection=conn),
         )
-        if !iszero(_term_connection_flow_lower_limit(m, conn, ng, d, path, t))
     )
 end
+
+
+"""
+    _connection_node_direction_for_min_flow(m)
+
+An iterator over tuples (connection, node, direction) for which a connection_flow_lower_limit is specified.
+If a capacity is specified for the same connection and node in the two directions and the 
+connection_flow_lower_limit is never zero, then the connection and node will be included in
+only one tuple and the direction will be a `Vector` of the two directions.
+In this case we can write a tight compact formulation.
+"""
+function _connection_node_direction_for_min_flow(m)
+    froms = indices(connection_capacity, connection__from_node)
+    tos = indices(connection_capacity, connection__to_node)
+    iter = Iterators.flatten((froms, tos))
+    if use_tight_compact_formulations(model=m.ext[:spineopt].instance)
+        bidirectional = intersect(((x.connection, x.node) for x in froms), ((x.connection, x.node) for x in tos))
+        filter!(x -> _is_never_zero(_from_lower_limit(x)) && _is_never_zero(_to_lower_limit(x)), bidirectional)
+        Iterators.flatten(
+            (
+                (x for x in iter if !((x.connection, x.node) in bidirectional)),
+                ((conn, n, direction()) for (conn, n) in bidirectional),
+            )
+        )
+    else
+        iter
+    end
+end
+
+_from_lower_limit(x) = connection_flow_lower_limit(; zip((:connection, :node), x)..., direction=direction(:from_node))
+_to_lower_limit(x) = connection_flow_lower_limit(; zip((:connection, :node), x)..., direction=direction(:to_node))
