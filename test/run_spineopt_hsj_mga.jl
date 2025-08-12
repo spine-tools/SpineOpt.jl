b22b83471c1f327083adebcda0a3c86b1651324b
Kacper Kozerski <kozerskikacper@gmail.com>
2025-08-12 16:29:14 +0200

    fixed the warning that suggests adding bridges

--- test/run_spineopt_hsj_mga.jl
+++ test/run_spineopt_hsj_mga.jl
@@ -276,7 +276,7 @@ function _test_run_spineopt_fuzzy_mga()
             object_parameter_values=object_parameter_values,
             relationship_parameter_values=relationship_parameter_values
         )
-        m = run_spineopt(url_in; log_level=1)
+        m = run_spineopt(url_in; log_level=1, add_bridges=true)
     end
     @testset "run_spineopt_fuzzy_mga" begin
         url_in = _test_run_spineopt_mga_setup()
@@ -286,7 +286,7 @@ function _test_run_spineopt_fuzzy_mga()
             object_parameter_values=object_parameter_values,
             relationship_parameter_values=relationship_parameter_values
         )
-        m = run_spineopt(url_in; log_level=1)
+        m = run_spineopt(url_in; log_level=1, add_bridges=true)
     end
     @testset "run_spineopt_fuzzy_mga_advanced" begin
         url_in = _test_run_spineopt_mga_setup()
@@ -296,7 +296,7 @@ function _test_run_spineopt_fuzzy_mga()
             object_parameter_values=object_parameter_values,
             relationship_parameter_values=relationship_parameter_values
         )
-        m = run_spineopt(url_in; log_level=1)
+        m = run_spineopt(url_in; log_level=1, add_bridges=true)
     end
 end
 
