0cd7573ad94ca43fe7d0b8c764ce1fb5272baed1
Tasqu <topi.rasku@vtt.fi>
2026-02-25 08:06:35 +0200

    Fix depreciation warnings.
    Or at least the ones I can find. For whatever reason, running the unit tests with `julia --depwarn=error` on crashes the REPL for some unit test sets.

--- src/run_spineopt.jl
+++ src/run_spineopt.jl
@@ -521,7 +521,7 @@ struct SpineOptExt
     has_results::Base.RefValue{Bool}
     event_handlers::Dict
     extras::Dict
-    function SpineOptExt(instance, lp_solver, master_model=nothing; model_by_stage=Dict(), stage=nothing)
+    function SpineOptExt(instance, lp_solver, master_model=nothing; model_by_stage=OrderedDict(), stage=nothing)
         intermediate_results_folder = if stage === nothing
             intermediate_results_folder = tempname(; cleanup=false)
             mkpath(intermediate_results_folder)
