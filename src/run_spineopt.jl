145b598413630dd04aeeeee0db97f52cf485019a
Huang, JiangYi <43530784+nnhjy@users.noreply.github.com>
2026-05-05 17:03:36 +0200

    Add run_spineopt model info into running logs (#1308)
    * sort active items log alphabatically
    
    * convert set into array for `sort` to work
    
    * restore unit tests for the functions for active item logging
    
    * log formatting

--- src/run_spineopt.jl
+++ src/run_spineopt.jl
@@ -160,9 +160,13 @@ function _run_spineopt(
         )
     f(m)
     run_spineopt!(m, url_out; log_level, alternative, kwargs...)
+    @log log_level 3 "\nSpineOpt model instance summary:"
+    log_level >= 3 && foreach(i -> print_active(m, i), [:variables, :objective_terms, :constraints])
+    @log log_level 3 "\nActive model outputs not included in the report:"
+    log_level >= 3 && foreach(println, hidden_active_outputs(m))
     t_end = now()
     elapsed_time_string = _elapsed_time_string(t_start, t_end)
-    @log log_level 1 "Execution complete. Started at $t_start, ended at $t_end, elapsed time: $elapsed_time_string"
+    @log log_level 1 "\nExecution complete. Started at $t_start, ended at $t_end, elapsed time: $elapsed_time_string"
     if url_out !== nothing
         stat_keys = [
             :SpineOpt_version, :SpineOpt_git_hash, :SpineInterface_version, :SpineInterface_git_hash, :elapsed_time
@@ -684,4 +688,38 @@ function _set_starting_point!(m, k=nothing)
             end
         end
     end
+end
+
+"""
+    print_active(m, field)
+
+Print active items of a field in the built SpineOpt model `m`.
+"""
+function print_active(m::JuMP.Model, field::Symbol)::Nothing
+    println("*** Active SpineOpt `$field`: ***")
+    foreach(println, active_spineopt_ext_items(m.ext[:spineopt], field))
+end
+
+"""
+    active_spineopt_ext_items(spineopt_ext, field)
+
+Active items of a field of an `SpineOptExt` instance.
+"""
+function active_spineopt_ext_items(spineopt_ext::SpineOptExt, field::Symbol)::Vector{Symbol}
+    items = getproperty(spineopt_ext, field)
+    sort([
+        key for key in keys(items) 
+        if !isnothing(items[key]) && !isempty(items[key]) && !isequal(items[key], (0, 0))
+    ])
+end
+
+"""
+     hidden_active_outputs(m)
+
+Active model outputs that are not reported
+"""
+function hidden_active_outputs(m::JuMP.Model)::Vector{Symbol}
+    spineopt_ext = m.ext[:spineopt]
+    hidden_outputs = setdiff(keys(spineopt_ext.values), keys(spineopt_ext.outputs))
+    return intersect(hidden_outputs, active_spineopt_ext_items(spineopt_ext, :values)) |> collect |> sort
 end
