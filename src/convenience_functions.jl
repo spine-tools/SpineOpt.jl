e9c3d223a24a11621d20ee35c0b154b95a451108
Diego Alejandro Tejada Arango <diego.tejadaarango@tno.nl>
2026-05-09 10:16:40 +0200

    Fix representative periods test

--- src/convenience_functions.jl
+++ src/convenience_functions.jl
@@ -17,7 +17,7 @@ const stage = ObjectClass(:stage)
 const stochastic_scenario = ObjectClass(:stochastic_scenario)
 const stochastic_structure = ObjectClass(:stochastic_structure)
 const temporal_block = ObjectClass(:temporal_block)
-const starting_point = ObjectClass(:starting_point)
+const starting_point = ObjectClass(:temporal_block)
 const unit = ObjectClass(:unit)
 const user_constraint = ObjectClass(:user_constraint)
 ## Relationship classes
